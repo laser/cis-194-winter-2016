de7c56142b0658493a09265584975cacee94c0e4
Erin Swenson-Healey <eswenson-healey@cj.com>
2015-12-24 15:00:36 -0800

    Revert "Run all specs from single file"
    This reverts commit ebe7a716d6b6096468748a4b5cf6636ed00ce129.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,1 @@
-module Spec where
-
-import Test.Hspec (hspec)
-
-import qualified Homework.Week1Spec as Week1
-import qualified Homework.Week2Spec as Week2
-
-main :: IO ()
-main = do
-  hspec Week1.spec
-  hspec Week2.spec
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
