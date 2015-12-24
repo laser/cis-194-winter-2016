ebe7a716d6b6096468748a4b5cf6636ed00ce129
Erin Swenson-Healey <eswenson-healey@cj.com>
2015-12-24 14:34:10 -0800

    Run all specs from single file

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,1 +1,11 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+module Spec where
+
+import Test.Hspec (hspec)
+
+import qualified Homework.Week1Spec as Week1
+import qualified Homework.Week2Spec as Week2
+
+main :: IO ()
+main = do
+  hspec Week1.spec
+  hspec Week2.spec
