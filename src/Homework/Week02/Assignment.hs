4441db022de9ea5b42853eefe6e824ad7b83d3fe
Bill Adams <badams@cj.com>
2016-01-08 11:01:39 -0800

    Assignment 2 - log msg parsing done

--- src/Homework/Week02/Assignment.hs
+++ src/Homework/Week02/Assignment.hs
@@ -13,13 +13,35 @@ module Homework.Week02.Assignment (
 
 import Homework.Week02.Log
 
+toTimeStamp :: String -> TimeStamp
+toTimeStamp  = read  
+
+parseInfo    ::  [String] -> LogMessage
+parseInfo (ts : rest ) =    LogMessage Info    (toTimeStamp ts)  (unwords rest)
+
+parseWarning :: [String] -> LogMessage
+parseWarning (ts : rest) =  LogMessage Warning (toTimeStamp ts)  (unwords rest)
+
+
+parseError   :: [String] -> LogMessage
+parseError (error : ts : rest )   =
+   LogMessage (Error errCode)
+              (toTimeStamp ts)
+              (unwords rest)
+   where errCode = read error :: Int
+
 -- #1a
 parseMessage :: String -> LogMessage
-parseMessage = undefined
-
+parseMessage line = case level of
+  "I" -> parseInfo    xs
+  "W" -> parseWarning xs
+  "E" -> parseError   xs
+  _   -> Unknown line
+  where (level : xs ) = words line
+                    
 -- #1b
 parse :: String -> [LogMessage]
-parse = undefined
+parse input = map parseMessage $ lines input
 
 -- #2
 insert :: LogMessage -> MessageTree -> MessageTree
