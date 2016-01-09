9405cb773cdfb1e6393ef5c457e9851f86b413da
hal arnold <halarnold2000@yahoo.com>
2016-01-09 08:40:52 -0800

    all tests pass for credit card

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -2,6 +2,15 @@ module Homework.Week01.Assignment where
 
 import Data.List (zipWith)
 
+div10 :: Integral a => a -> a
+div10 n = n `div` 10
+
+rem10 :: Integral a => a -> a
+rem10 n
+  | n <= 0 = 0
+  | n > 0  = n `rem` 10
+
+
 -- #1a
 toDigits :: Integer -> [Integer]
 toDigits n
@@ -9,6 +18,8 @@ toDigits n
    | n > 0  = toDigits divBy10 ++ [val]
     where val      = n `rem` 10
           divBy10  = n `div` 10
+
+-- helper
 revList :: [Integer] -> [Integer]
 revList = foldl (flip (:)) []
 
@@ -25,13 +36,27 @@ doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther = revList . zipWith (*) multiplier . revList
   where multiplier = concat $ replicate 20 [1,2]
 
--- #3
+-- helper
+
+-- destructVal 12 -> [1,2]
+-- destructVal 1  -> [1]
+destructVal :: Integer ->[Integer]
+destructVal n
+  | n <= 0      =  [0]
+  | n < 10      = [rem10 n]
+  | n == 10     = [1,0]
+  | n > 10      = div10 n : [rem10 n]
+
+
+
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits xs   = sum  (concatMap destructVal xs)
 
 -- #4
+
 validate :: Integer -> Bool
-validate = undefined
+validate n = validate' n  == 0 where
+  validate'  = rem10 . sumDigits . doubleEveryOther . toDigits
 
 -- #5
 type Peg = String
