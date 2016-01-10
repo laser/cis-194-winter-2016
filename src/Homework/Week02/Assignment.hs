76ff4b5b775a0818789f834ec192b8a575dd78c9
hal arnold <halarnold2000@yahoo.com>
2016-01-10 12:36:29 -0800

    #2, up thru parse

--- src/Homework/Week02/Assignment.hs
+++ src/Homework/Week02/Assignment.hs
@@ -26,23 +26,9 @@ parseErrorMessage s =
       str = unwords rest
   in LogMessage (Error cd) cmsg str
 
---for Info: "I 562 yo, dawg"
--- parseInfoMessage :: String -> LogMessage
--- parseInfoMessage s =
---   let (_:imsg:rest) = words s
---       cmsg = read imsg :: Int
---       str = unwords rest
---   in LogMessage Info cmsg str
-
--- parseWarningMessage :: String -> LogMessage
--- parseWarningMessage s =
---   let (_:wmsg:rest) = words s
---       cmsg = read wmsg :: Int
---       str = unwords rest
---   in LogMessage Warning cmsg str
-
-parseTypeMessage :: String -> LogMessage
-parseTypeMessage s =
+-- all non error type messages
+parseNonErrorMessage :: String -> LogMessage
+parseNonErrorMessage s =
   let (t:msg:rest) = words s
       msg' = read msg :: Int
       str = unwords rest
@@ -51,28 +37,17 @@ parseTypeMessage s =
         "W" -> Warning
   in LogMessage type' msg' str
 
-
-  --for Warning: "W 562 yo, dawg"
-  -- parseWarnMessage s = undefined
-  --   let (_:wmsg:rest) = words s
-  --       cmsg = read wmsg :: Int
-  --       str = unwords rest
-  --   in LogMessage Warning wmsg str
-
 parseMessage :: String -> LogMessage
 parseMessage s = case s of
 
   ('E' : rest) -> parseErrorMessage s
-  ('I' : rest) -> parseTypeMessage s
-  ('W' : rest) -> parseTypeMessage s
+  ('I' : rest) -> parseNonErrorMessage s
+  ('W' : rest) -> parseNonErrorMessage s
   _ -> Unknown s
-
-
---parseMessage (LogMessage MessageType ts str)
 
 -- #1b
 parse :: String -> [LogMessage]
-parse = undefined
+parse = map parseMessage . lines
 
 -- #2
 insert :: LogMessage -> MessageTree -> MessageTree
