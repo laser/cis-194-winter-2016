444df334b04e8ee972b9d1cd043535b4f6020389
Abdussalam Abdurrahman <abdusalam@live.com>
2016-01-13 12:17:14 -0800

    homework 1, exercise 5 (for real)

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -36,7 +36,7 @@ type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _ = []
-hanoi 1 a b c = [(a, b)]
+hanoi x a b c = (hanoi (x - 1) a c b) ++ [(a, b)] ++ (hanoi (x - 1) c b a)
 
 hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
 hanoi4 = undefined
