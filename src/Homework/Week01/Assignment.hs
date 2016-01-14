47824aa6d16fbaca307305e01ef5d93f421a1692
zhuochengyu <zyu@cj.com>
2016-01-13 19:56:22 -0800

    clean up my file

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -1,54 +1,18 @@
 module Homework.Week01.Assignment where
 -- #1a
+toDigits :: Integer -> [Integer]
+toDigits = reverse . toDigitsRev
+
+-- #1b
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev n
     | n <= 0 = []
     | otherwise = (n `mod` 10) : toDigitsRev(n `div` 10)
 
--- #1b
-toDigits :: Integer -> [Integer]
-toDigits = reverse . toDigitsRev
-
---first attempt
--- returnADoubleValueIfIndexIsEvenElseReturnSameValueFromList :: Int -> [Integer] -> Integer
--- returnADoubleValueIfIndexIsEvenElseReturnSameValueFromList n list
---     | n `mod` 2 == 1 = 2 * (head $ drop n list)
---     | otherwise = head $ drop n list
---
--- -- #2
--- doubleEveryOtherFromLeft :: [Integer] -> (Int -> [Integer] -> Integer) -> [Integer]
--- doubleEveryOtherFromLeft list accFunc = foldr (\x y -> accFunc x list : y) [] [0 .. length list - 1]
---
--- doubleEveryOther :: [Integer] -> [Integer]
--- doubleEveryOther list = reverse $ doubleEveryOtherFromLeft (reverse list) returnADoubleValueIfIndexIsEvenElseReturnSameValueFromList
-
---second attempt
--- getListWithIndices :: [Integer] -> [(Integer, Integer)]
--- getListWithIndices list = zip list [0 .. toInteger $ length list - 1]
---
--- returnIntegerTwoIfItsOdd :: Integer -> Integer
--- returnIntegerTwoIfItsOdd n
---   | odd n = 2
---   | otherwise = 1
---
--- doubleIfIndexIsEven :: (Integer, Integer) -> Integer
--- doubleIfIndexIsEven pair
---   = let (digit, index) = pair
---     in digit * returnIntegerTwoIfItsOdd index
---
---
--- doubleEveryOtherFromLeft :: [Integer] -> [Integer]
--- doubleEveryOtherFromLeft list = map doubleIfIndexIsEven (getListWithIndices list)
---
--- doubleEveryOther :: [Integer] -> [Integer]
--- doubleEveryOther list = reverse $ doubleEveryOtherFromLeft (reverse list)
-
--- Solution that is similar to the chapter 1's sumEveryTwo
-
+-- #2
 doubleEveryOtherFromLeft :: [Integer] -> [Integer]
-doubleEveryOtherFromLeft [] = []
-doubleEveryOtherFromLeft (x : []) = [x]
 doubleEveryOtherFromLeft (x : y : z) = x : (2 * y) : doubleEveryOtherFromLeft z
+doubleEveryOtherFromLeft list = list
 
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther list  = reverse $ doubleEveryOtherFromLeft (reverse list)
@@ -56,6 +20,7 @@ doubleEveryOther list  = reverse $ doubleEveryOtherFromLeft (reverse list)
 -- #3
 sumDigits :: [Integer] -> Integer
 sumDigits list = sum $ concatMap toDigits list
+
 -- #4
 validate :: Integer -> Bool
 validate = (== 0 ) . (`mod` 10) . sumDigits . doubleEveryOther . toDigits
