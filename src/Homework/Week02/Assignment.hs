d8a7afad69138ddb48b8bfbc9b25a3d9a4ae2bcc
zhuochengyu <zyu@cj.com>
2016-01-14 21:47:04 -0800

    finish 2 1b

--- src/Homework/Week02/Assignment.hs
+++ src/Homework/Week02/Assignment.hs
@@ -13,9 +13,19 @@ module Homework.Week02.Assignment (
 
 import Homework.Week02.Log
 
+-- data LogMessage = LogMessage MessageType TimeStamp String
+--                 | Unknown String
+
+--  parseMessage "E 2 562 help help" `shouldBe` LogMessage (Error 2) 562 "help help"
+
 -- #1a
+parseMessageList :: [String] -> LogMessage
+parseMessageList ("E" : messageType : timeStamp : message) = LogMessage (Error (read messageType :: Int)) (read timeStamp :: Int)  (unwords message)
+parseMessageList ("I" : messageType : message) = LogMessage Info (read messageType :: Int)  (unwords message)
+parseMessageList unknown = Unknown (unwords unknown)
+
 parseMessage :: String -> LogMessage
-parseMessage = undefined
+parseMessage = parseMessageList . words
 
 -- #1b
 parse :: String -> [LogMessage]
