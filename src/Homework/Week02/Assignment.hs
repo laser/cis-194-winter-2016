c3ab76e80fcc0f129c9b93c927f87ae0d9770437
Erin Swenson-Healey <eswenson-healey@cj.com>
2016-01-15 12:57:55 -0800

    Implement Week 2 assignment

--- src/Homework/Week02/Assignment.hs
+++ src/Homework/Week02/Assignment.hs
@@ -5,34 +5,53 @@ module Homework.Week02.Assignment (
   parse,
   parseMessage,
   whatWentWrong,
+  readTheLogAndReport,
   LogMessage(..),
   MessageTree(..),
   MessageType(..),
   TimeStamp
 ) where
 
-import Homework.Week02.Log
+import           Control.Monad       (liftM)
+
+import           Homework.Week02.Log
 
 -- #1a
 parseMessage :: String -> LogMessage
-parseMessage = undefined
+parseMessage s = case words s of
+  ("W":n:xs)     -> LogMessage Warning (read n) (unwords xs)
+  ("I":n:xs)     -> LogMessage Info (read n) (unwords xs)
+  ("E":n1:n2:xs) -> LogMessage (Error (read n1)) (read n2) (unwords xs)
+  (_)            -> Unknown s
 
 -- #1b
 parse :: String -> [LogMessage]
-parse = undefined
+parse = map parseMessage . lines
 
 -- #2
 insert :: LogMessage -> MessageTree -> MessageTree
-insert = undefined
+insert (Unknown _) t = t
+insert m Leaf = Node Leaf m Leaf
+insert m1@(LogMessage _ ts1 _) (Node lt m2@(LogMessage _ ts2 _) rt) =
+  let i = insert m1
+      z = flip Node m2
+  in if ts1 < ts2 then z (i lt) rt
+                  else z lt (i rt)
 
 -- #3
 build :: [LogMessage] -> MessageTree
-build = undefined
+build = foldl (flip insert) Leaf
 
 -- #4
 inOrder :: MessageTree -> [LogMessage]
-inOrder = undefined
+inOrder Leaf = []
+inOrder (Node lt m rt) = (inOrder lt) ++ [m] ++ (inOrder rt)
 
 -- #5
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong = undefined
+whatWentWrong = foldr k [] . inOrder . build
+  where k (LogMessage (Error n) _ s) acc = if n > 49 then s:acc else acc
+        k _ acc = acc
+
+readTheLogAndReport :: FilePath -> IO [String]
+readTheLogAndReport = liftM (whatWentWrong . parse) . readFile
