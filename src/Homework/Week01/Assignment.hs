23e47cb95f0c34a3f2dcf1d6e82e6429c8f050f1
m-arnold <michaelaarnold@gmail.com>
2016-01-14 19:33:07 -0800

    hw 1: cc validation

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -2,23 +2,40 @@ module Homework.Week01.Assignment where
 
 -- #1a
 toDigits :: Integer -> [Integer]
-toDigits = undefined
+toDigits 0 = []
+toDigits n
+  | n < 0     = []
+  | otherwise = toDigits (n `div` 10) ++ [n `mod` 10]
 
 -- #1b
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = undefined
+toDigitsRev 0 = []
+toDigitsRev n
+  | n < 0     = []
+  | otherwise = n `mod` 10 : toDigitsRev (n `div` 10)
 
 -- #2
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther []       = []
+doubleEveryOther (x:[])   = [x]
+doubleEveryOther (x:xs)   = (reverse . doubleEveryOtherFromLeft . reverse) (x:xs)
+
+doubleEveryOtherFromLeft :: [Integer] -> [Integer]
+doubleEveryOtherFromLeft []       = []
+doubleEveryOtherFromLeft [x]      = [x]
+doubleEveryOtherFromLeft (x:y:xs) = x : (y*2) : doubleEveryOtherFromLeft xs
 
 -- #3
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits []      = 0
+sumDigits [x]
+  | (x < 10)  = x
+  | otherwise = (sumDigits . toDigits) x
+sumDigits (x:xs)  = sumDigits [x] + sumDigits xs
 
 -- #4
 validate :: Integer -> Bool
-validate = undefined
+validate n = (sumDigits . doubleEveryOther . toDigits) n `mod` 10 == 0
 
 -- #5
 type Peg = String
