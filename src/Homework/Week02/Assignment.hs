328a29221d88dca38e6db7c74d4db8c2223dc1cf
m-arnold <michaelaarnold@gmail.com>
2016-01-17 11:37:00 -0800

    working on hw 2

--- src/Homework/Week02/Assignment.hs
+++ src/Homework/Week02/Assignment.hs
@@ -15,19 +15,30 @@ import Homework.Week02.Log
 
 -- #1a
 parseMessage :: String -> LogMessage
-parseMessage = undefined
+parseMessage message = case message of
+  ('I':s) -> LogMessage Info (read (head (words s))::Int) (unwords (drop 1 (words s)))
+  ('W':s) -> LogMessage Warning (read (head (words s))::Int) (unwords (drop 1 (words s)))
+  ('E':s) -> LogMessage (Error (read (head (words s))::Int)) (read (head (drop 1 (words s)))::Int) (unwords (drop 2 (words s)))
+  _       -> Unknown message
 
 -- #1b
 parse :: String -> [LogMessage]
-parse = undefined
+parse messageLog = map parseMessage (lines messageLog)
 
 -- #2
 insert :: LogMessage -> MessageTree -> MessageTree
-insert = undefined
+insert (Unknown _) tree = tree
+insert message Leaf     = Node Leaf message Leaf
+insert message@(LogMessage mtype insertTime rest) tree@(Node left contents@(LogMessage _ treeTime _) right)
+  | insertTime <= treeTime  = Node (insert message left) contents right
+  | otherwise               = Node left contents (insert message right)
 
 -- #3
 build :: [LogMessage] -> MessageTree
-build = undefined
+build messages = case messages of
+  [m]    -> insert m Leaf
+  (m:ms) -> insert m (build ms)
+  _      -> Leaf
 
 -- #4
 inOrder :: MessageTree -> [LogMessage]
