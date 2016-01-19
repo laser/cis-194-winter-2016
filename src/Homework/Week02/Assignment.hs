70aaaa33215607d97d2eca89d8b32d1505ac4eef
jshah <jshah@cj.com>
2016-01-19 13:49:22 -0800

    working parse and refactor parseMessage

--- src/Homework/Week02/Assignment.hs
+++ src/Homework/Week02/Assignment.hs
@@ -17,14 +17,18 @@ import Debug.Trace
 -- #1a
 parseMessage :: String -> LogMessage
 parseMessage (s) =  (case words(s) of
-                        ("E" : code : time : xs) -> LogMessage (Error (read(code))) (read(time)) (unwords(xs))
-                        ("I" : time : xs) -> LogMessage Info (read(time)) (unwords(xs))
+                        ("E" : code : time : xs) -> LogMessage (Error (read code)) (read time) (unwords xs)
+                        ("I" : time : xs) -> LogMessage Info (read time) (unwords xs)
                         xs -> Unknown (unwords xs))
 
 -- trace ("s is " ++ s)
 -- #1b
 parse :: String -> [LogMessage]
-parse = undefined
+parse = parseLines . lines
+
+parseLines :: [String] -> [LogMessage]
+parseLines (x:[]) = parseMessage(x) : []
+parseLines (x:xs) = parseMessage(x) : parseLines(xs)
 
 -- #2
 insert :: LogMessage -> MessageTree -> MessageTree
