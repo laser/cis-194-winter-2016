f2947a4a66500d9caec96f53c86a312976ddb686
Hitesh Lad <hlad@cj.com>
2016-01-18 17:12:47 -0800

    homework 1 - cc validation

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -2,23 +2,40 @@ module Homework.Week01.Assignment where
 
 -- #1a
 toDigits :: Integer -> [Integer]
-toDigits = undefined
+toDigits n = reverse (toDigitsRev n)
 
 -- #1b
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = undefined
+toDigitsRev n
+  | n <= 0    =  []
+  | otherwise =  n `mod` 10 : toDigitsRev (n `div` 10)
 
 -- #2
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = undefined
+doubleEveryOther xs = reverse (doubleEveryOtherRev (reverse xs))
+
+doubleEveryOtherRev :: [Integer] -> [Integer]
+doubleEveryOtherRev []              = []
+doubleEveryOtherRev [x]             = [ x ]
+doubleEveryOtherRev [x, y]          = [ x, 2 * y ]
+doubleEveryOtherRev (x : y : zs)    = [ x, 2 * y ] ++ doubleEveryOtherRev (zs)
+
 
 -- #3
 sumDigits :: [Integer] -> Integer
-sumDigits = undefined
+sumDigits []       = 0
+sumDigits (x : ys) = sumDigit x + sumDigits ys
+
+sumDigit :: Integer -> Integer
+sumDigit n
+  | n <= 0    = 0
+  | n < 10    = n
+  | otherwise = 1 + sumDigit (n - 10)
+
 
 -- #4
 validate :: Integer -> Bool
-validate = undefined
+validate n = sumDigits( doubleEveryOther ( toDigits n )) `mod` 10 == 0
 
 -- #5
 type Peg = String
