fe8d52455d4160d1f7cd01349b65b9810c80a136
Hitesh Lad <hlad@cj.com>
2016-01-18 17:41:17 -0800

    homework 1 - hanoi 3

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -42,7 +42,8 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi = undefined
+hanoi 1 source target temp = [(source, target)]
+hanoi n source target temp = (hanoi (n-1) source temp target) ++ [(source, target)] ++ (hanoi (n-1) temp target source)
 
 hanoi4 :: Integer -> Peg -> Peg -> Peg -> Peg -> [Move]
 hanoi4 = undefined
