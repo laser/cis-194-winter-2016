ce8bad73a7e0cb04197becbee49267a7e576b2b3
Hitesh Lad <hlad@cj.com>
2016-01-19 12:11:35 -0800

    support hanoi with 0 disks

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -42,6 +42,7 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 source target temp = []
 hanoi 1 source target temp = [(source, target)]
 hanoi n source target temp = (hanoi (n-1) source temp target) ++ [(source, target)] ++ (hanoi (n-1) temp target source)
 
