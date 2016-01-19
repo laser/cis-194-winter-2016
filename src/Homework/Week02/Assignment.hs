ff31069c94135bb292601c0a06c2a5fff64e2c18
m-arnold <michaelaarnold@gmail.com>
2016-01-18 16:01:38 -0800

    week 2 hw

--- src/Homework/Week02/Assignment.hs
+++ src/Homework/Week02/Assignment.hs
@@ -15,11 +15,11 @@ import Homework.Week02.Log
 
 -- #1a
 parseMessage :: String -> LogMessage
-parseMessage message = case message of
-  ('I':s) -> LogMessage Info (read (head (words s))::Int) (unwords (drop 1 (words s)))
-  ('W':s) -> LogMessage Warning (read (head (words s))::Int) (unwords (drop 1 (words s)))
-  ('E':s) -> LogMessage (Error (read (head (words s))::Int)) (read (head (drop 1 (words s)))::Int) (unwords (drop 2 (words s)))
-  _       -> Unknown message
+parseMessage msg = case words msg of
+  ("I":n:rest)      -> LogMessage Info (read n::Int) (unwords rest)
+  ("W":n:rest)      -> LogMessage Warning (read n::Int) (unwords rest)
+  ("E":sev:n:rest)  -> LogMessage (Error (read sev::Int)) (read n::Int) (unwords rest)
+  (_)               -> Unknown msg
 
 -- #1b
 parse :: String -> [LogMessage]
@@ -29,21 +29,30 @@ parse messageLog = map parseMessage (lines messageLog)
 insert :: LogMessage -> MessageTree -> MessageTree
 insert (Unknown _) tree = tree
 insert message Leaf     = Node Leaf message Leaf
-insert message@(LogMessage mtype insertTime rest) tree@(Node left contents@(LogMessage _ treeTime _) right)
-  | insertTime <= treeTime  = Node (insert message left) contents right
-  | otherwise               = Node left contents (insert message right)
+insert message@(LogMessage _ newTime _) tree@(Node left oldMessage@(LogMessage _ oldTime _) right)
+  | newTime <= oldTime  = Node (insert message left) oldMessage right
+  | otherwise           = Node left oldMessage (insert message right)
 
 -- #3
 build :: [LogMessage] -> MessageTree
-build messages = case messages of
-  [m]    -> insert m Leaf
-  (m:ms) -> insert m (build ms)
-  _      -> Leaf
+-- build messages = case messages of
+--   (m:[]) -> insert m Leaf
+--   (m:ms) -> insert m (build ms)
+--   _      -> Leaf
+build = foldl (flip insert) Leaf
 
 -- #4
 inOrder :: MessageTree -> [LogMessage]
-inOrder = undefined
+inOrder Leaf = []
+inOrder tree@(Node left contents right)  = (inOrder left) ++ [contents] ++ (inOrder right)
 
 -- #5
+errors50AndUp :: LogMessage -> Bool
+errors50AndUp (LogMessage (Error sev) _ _) = sev > 49
+errors50AndUp _                            = False
+
+getMsg :: LogMessage -> String
+getMsg (LogMessage _ _ msg) = msg
+
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong = undefined
+whatWentWrong = (map getMsg) . (filter errors50AndUp) . inOrder . build
