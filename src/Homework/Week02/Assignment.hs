5d2e9b7863fe59fd73e9051c17587fb2cbe8edaa
Nicolas Font <nfont@cj.com>
2016-01-19 21:30:03 -0800

    Week 2

--- src/Homework/Week02/Assignment.hs
+++ src/Homework/Week02/Assignment.hs
@@ -15,24 +15,40 @@ import Homework.Week02.Log
 
 -- #1a
 parseMessage :: String -> LogMessage
-parseMessage = undefined
+parseMessage s = case words s of
+  ("W":timestamp:message) -> LogMessage Warning (read timestamp::Int) (unwords message)
+  ("I":timestamp:message) -> LogMessage Info (read timestamp::Int) (unwords message)
+  ("E":level:timestamp:message) -> LogMessage (Error (read level::Int)) (read timestamp::Int) (unwords message)
+  (_) -> Unknown s
 
 -- #1b
 parse :: String -> [LogMessage]
-parse = undefined
+parse s = map parseMessage (lines s)
 
 -- #2
 insert :: LogMessage -> MessageTree -> MessageTree
-insert = undefined
+insert (Unknown _) tree = tree
+insert message Leaf = Node Leaf message Leaf
+insert message (Node a rootMessage b)
+  | older message rootMessage = Node (insert message a) rootMessage b
+  | otherwise                 = Node a rootMessage (insert message b)
+
+older :: LogMessage -> LogMessage -> Bool
+older (LogMessage _ ts1 _) (LogMessage _ ts2 _) = ts1 < ts2
 
 -- #3
 build :: [LogMessage] -> MessageTree
-build = undefined
+build messages = foldr insert Leaf (reverse messages)
 
 -- #4
 inOrder :: MessageTree -> [LogMessage]
-inOrder = undefined
+inOrder Leaf = []
+inOrder (Node left message right) = (inOrder left) ++ [message] ++ (inOrder right)
 
 -- #5
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong = undefined
+whatWentWrong ms = map (\(LogMessage _ _ m) -> m) (inOrder $ build $ filter isRelevantError ms)
+
+isRelevantError :: LogMessage -> Bool
+isRelevantError (LogMessage (Error severity) _ _) = severity > 50
+isRelevantError _ = False
