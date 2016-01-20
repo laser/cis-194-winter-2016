65ebc0c6755799fb3cb4ea758885712087471423
Vivek Sachdeva <vsachdeva@cj.com>
2016-01-19 19:01:07 -0800

    Implement all the functions

--- src/Homework/Week02/Assignment.hs
+++ src/Homework/Week02/Assignment.hs
@@ -13,26 +13,48 @@ module Homework.Week02.Assignment (
 
 import Homework.Week02.Log
 
--- #1a
 parseMessage :: String -> LogMessage
-parseMessage = undefined
+parseMessage (message)
+    | head ws == ['E'] = LogMessage (Error (read (ws !! 1)::Int)) (read (ws !! 2)) (unwords (drop 3 ws))
+    | head ws == ['I'] = LogMessage Info (read (ws !! 1)::Int) (unwords (drop 2 ws))
+    | head ws == ['W'] = LogMessage Warning (read (ws !! 1)::Int) (unwords (drop 2 ws))
+    | otherwise = Unknown "This is not in the right format"
+    where ws = words (message)
 
 -- #1b
 parse :: String -> [LogMessage]
-parse = undefined
+parse (multiLineLog) = map parseMessage (lines(multiLineLog))
 
 -- #2
 insert :: LogMessage -> MessageTree -> MessageTree
-insert = undefined
+insert (Unknown _) tree = tree
+insert msg Leaf = Node Leaf msg Leaf
+insert (toBeAdded@(LogMessage msgType timeToBeAdded msgString)) (Node l root@(LogMessage _ time _) r) =
+    if (timeToBeAdded < time)
+      then Node (insert (toBeAdded) l) root r
+    else
+      Node l root (insert (toBeAdded) r)
 
 -- #3
 build :: [LogMessage] -> MessageTree
-build = undefined
+build logMessages = foldl (flip insert) Leaf logMessages
 
 -- #4
 inOrder :: MessageTree -> [LogMessage]
-inOrder = undefined
+inOrder Leaf = []
+inOrder (Node left msg right) = inOrder left ++ [msg] ++ inOrder right
+
+isRelevantError :: LogMessage -> Bool
+isRelevantError (LogMessage (Error severity) _ _)
+    | severity >= 50 = True
+    | otherwise = False
+isRelevantError _  = False
 
 -- #5
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong = undefined
+whatWentWrong logMessages =
+  let relevantErrorMessages = filter (isRelevantError) logMessages
+      messageTree = build relevantErrorMessages
+      orderedLogMessages = inOrder messageTree
+      messages = map (\msg@(LogMessage msgType timeToBeAdded msgString) -> msgString) orderedLogMessages
+  in messages
