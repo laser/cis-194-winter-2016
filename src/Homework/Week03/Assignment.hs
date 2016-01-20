a6857933456f2fd7bd6e5da358b9bce05eb78d91
Bill Adams <badams@cj.com>
2016-01-20 14:49:33 -0800

    Assignment 3 rough draft

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -4,6 +4,8 @@ module Homework.Week03.Assignment (
   histogram
 ) where
 
+import Data.List
+
 -- #1
 skips :: [a] -> [[a]]
 skips xss = takeWhile (\x -> case x of
@@ -14,12 +16,38 @@ skips xss = takeWhile (\x -> case x of
 
 -- #2
 localMaxima :: [Integer] -> [Integer]
---localMaxima (a:b:c:xs)
---  |   a < b < c    = b : localMaxima (c : xs)
---  |   otherwise    = localMaxima (c : xs)                     
+localMaxima (a:b:c:xs)
+  | a < b && b > c = b : localMaxima (c : xs)
+  | otherwise      = localMaxima (b : c : xs)                     
 localMaxima _      = []
 
 
 -- #3
 histogram :: [Integer] -> String
-histogram = undefined
+histogram xs  =  lines ++ tallyLine ++ legend ++ ['\n']
+   where lines = foldr (++) "" $ reverse $ doRun $ group $ sort xs
+         tallyLine  = (take 10 $ repeat '=') ++ ['\n']
+         legend     = concat $ map show $ take 10 $ [0..]
+
+emptyLine :: String
+emptyLine = (take 10 $ repeat ' ') ++   ['\n']
+          
+doRun ::  [[Integer]] -> [String]
+doRun [] = []
+doRun xsxs =  currLine : (doRun next)
+    where (curr,next) = scrapeTheTop xsxs
+          currLine    = insertSequence emptyLine curr
+
+scrapeTheTop :: Eq t => [[t]] -> ([t], [[t]])
+scrapeTheTop xsxs = (concat curr, filter (/= []) next)
+             where (curr,next) =  juxt (take 1) (drop 1) xsxs
+                   
+insertSequence :: Foldable t => String ->  t Integer -> String
+insertSequence line = foldl insertInLine line
+
+insertInLine :: String -> Integer -> String
+insertInLine line idx = front ++ ['*'] ++ (drop 1 back)
+  where (front,back) = splitAt (fromIntegral idx)  line
+
+juxt :: (a -> b) -> (a -> b1) -> [a]-> ([b], [b1])           
+juxt f1 f2 xs = (map f1 xs,map f2 xs)
