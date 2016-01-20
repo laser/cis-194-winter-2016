bcdc370bdb127a9a064c146f707b57e664734923
zhuochengyu <zyu@cj.com>
2016-01-20 00:02:56 -0800

    week03 finished first two problems but lost my style point

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -4,13 +4,20 @@ module Homework.Week03.Assignment (
   histogram
 ) where
 
+getSingleWord :: [a] -> [Int] -> [a]
+getSingleWord list =  map (\x -> list !! x) -- OMG!! I don't have style
+
 -- #1
 skips :: [a] -> [[a]]
-skips = undefined
+skips [] = []
+skips list = foldr (\x y -> getSingleWord list ( takeWhile ( < length list ) $ foldr (:) [] (iterate (+(x+1)) x)) : y) [] [0 .. (length list - 1)]
 
 -- #2
 localMaxima :: [Integer] -> [Integer]
-localMaxima = undefined
+localMaxima (x:y:z:xs) = case y > x && y > z of
+  True -> y : localMaxima (z:xs)
+  otherwise -> localMaxima (y:z:xs)
+localMaxima doesntmatch = []
 
 -- #3
 histogram :: [Integer] -> String
