c550b25df6058a0796b43be0c749b2c56e8fd8ac
Bill Adams <badams@cj.com>
2016-01-21 06:57:05 -0800

    Assignment 4 - rough draft

--- src/Homework/Week04/Assignment.hs
+++ src/Homework/Week04/Assignment.hs
@@ -25,14 +25,14 @@ import Homework.Week04.BST
 import Data.Char
 import Data.List
 import Data.Maybe
-import qualified Data.Function as F
+
 -- #1
 ex1 :: a -> b -> b
-ex1 _ b = b
+ex1 a b = b
 
 -- #2
 ex2 :: a -> a -> a
-ex2 a _ = a
+ex2 a b = a
 
 -- #3
 ex3 :: Int -> a -> a
@@ -40,20 +40,19 @@ ex3 _ a = a
 
 -- #4
 ex4 :: Bool -> a -> a -> a
-ex4 _ a _ = a
+ex4 _ a b = a
 
 -- #5
 ex5 :: Bool -> Bool
-ex5 = id
+ex5 = id 
 
 -- #6
 ex6 :: (a -> a) -> a
-ex6 = F.fix
---ex6 = error "With one weird trick you can get the above"
+ex6 f = error "impossible"
 
 -- #7
 ex7 :: (a -> a) -> a -> a
-ex7  = id
+ex7  = id 
 
 -- #8
 ex8 :: [a] -> [a]
@@ -61,28 +60,29 @@ ex8 = id
 
 -- #9
 ex9 :: (a -> b) -> [a] -> [b]
-ex9 = map
+ex9 f = map f 
 
 -- #10
 ex10 :: Maybe a -> a
-ex10  = error "unverfuegbar"
+ex10  = error "impossible"
 
 -- #11
 ex11 :: a -> Maybe a
-ex11 = Just
+ex11 = Just 
 
 -- #12
 ex12 :: Maybe a -> Maybe a
-ex12  = id
+ex12 ma = ma
 
 -- #13
 insertBST :: (a -> a -> Ordering) -> a -> BST a -> BST a
 insertBST _ el Leaf = Node Leaf el Leaf
-insertBST cmp el tree@(Node left visit right) =
-  case cmp el visit of
+insertBST cmp el tree@(Node left visit right) = 
+  case (cmp el visit) of
      EQ -> tree
      LT -> insertBST cmp el left
      GT -> insertBST cmp el right
+    
 
 safeHead :: [a] -> Maybe a
 safeHead []     = Nothing
@@ -92,35 +92,18 @@ safeTail :: [a] -> Maybe [a]
 safeTail [] = Nothing
 safeTail (_ : xs) = Just xs
 
-
 -- #14
 allCaps :: [String] -> Bool
-
-allCaps  xs = all (\f->f xs) [allCaps0, allCaps1, allCaps2,  allCaps3]
-
-allCaps0 :: [String] -> Bool
-allCaps0 = all (\x -> x /= [] && all isUpper x ) . map (take 1)
-
-allCaps1 :: [String] -> Bool
-allCaps1 xs = length xs == length (takeWhile (==True) $ mapMaybe (safeHead . map isUpper) xs)
-
-allCaps2 :: [String] -> Bool
-allCaps2 = all (\ cx -> (cx /= []) && isUpper (head cx))
-
-allCaps3 :: [String] -> Bool
-allCaps3 xs = notElem "" xs && all (all isUpper) ( map (take 1) xs)
+allCaps xs = length xs == (length $ takeWhile (==True) $  catMaybes $  map (safeHead . map isUpper) xs)
 
 -- #15
 dropTrailingWhitespace :: String -> String
 dropTrailingWhitespace = reverse . dropWhile (== ' ' ) . reverse
 
-dropTrailingWhitespace' :: String -> String
-dropTrailingWhitespace' = dropWhileEnd isSpace
-
 -- #16
-firstLetters :: [String] -> String
-firstLetters = mapMaybe safeHead
+firstLetters :: [String] -> [Char]
+firstLetters = catMaybes . map safeHead
 
 -- #17
 asList :: [String] -> String
-asList xs = "[" ++ intercalate "," xs  ++ "]"
+asList xs = "[" ++ ( concat $ intersperse ", " xs ) ++ "]"
