b87de9f98df1759a0e39c5f01f8324c5d859eef2
Bill Adams <badams@cj.com>
2016-01-21 11:13:49 -0800

    refactor Assignment 4, start Assignment 5

--- src/Homework/Week05/Assignment.hs
+++ src/Homework/Week05/Assignment.hs
@@ -1,15 +1,43 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Homework.Week05.Assignment (
   eval,
   evalStr,
   ExprT(..),
-  -- uncomment these once you've defined them:
-  -- Expr(..),
-  -- MinMax(..),
-  -- Mod7(..)
+  Expr(..),
+  MinMax(..),
+  Mod7(..)
+ ,Listable(..)
 ) where
 
 import Homework.Week05.ExprT
 import Homework.Week05.Parser
+import Data.Char
+
+f :: a -> b
+f a = error "naaah gaaah daaah"
+
+
+class Listable a where
+  toList :: a -> [Int]
+
+instance Listable Int where
+  toList x = [x]
+
+instance Listable Bool where
+  toList True  = [1]
+  toList False = [0]
+
+instance Listable [Bool] where
+  toList = concatMap toList
+
+instance Listable [Int] where
+  toList = id
+
+instance Listable String where
+  toList = map ord
+
+instance (Listable a , Listable b) => Listable (a,b) where
+  toList (a,b) = toList a ++ toList b
 
 -- #1
 eval :: ExprT -> Integer
