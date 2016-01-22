75dd9c4a1b6b8fc447314e76511470974599a84f
mwilliams <mwilliams@cj.com>
2016-01-21 21:24:21 -0800

    week 5 first 2 functions with tests

--- src/Homework/Week05/Assignment.hs
+++ src/Homework/Week05/Assignment.hs
@@ -5,14 +5,20 @@ module Homework.Week05.Assignment (
 ) where
 
 import Homework.Week05.ExprT
+import Homework.Week05.Parser
 
 -- #1
 eval :: ExprT -> Integer
-eval = undefined
+eval e = case e of
+    Lit n -> n
+    Add e1 e2 -> eval e1 + eval e2
+    Mul e1 e2 -> eval e1 * eval e2
 
 -- #2
 evalStr :: String -> Maybe Integer
-evalStr = undefined
+evalStr s = case parseExp Lit Add Mul s of
+    Just expr -> Just (eval expr)
+    Nothing   -> Nothing
 
 -- #3
 -- class Expr a where 
--- test/Homework/Week05Spec.hs
+++ test/Homework/Week05Spec.hs
@@ -13,5 +13,14 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "week 5" $ do
-    it "needs some tests!" $ do
-      pending
+    it "should eval an expression" $ do
+      eval (Add (Lit 2) (Lit 3)) `shouldBe` 5
+      eval (Mul (Lit 2) (Lit 3)) `shouldBe` 6
+      eval (Mul (Add (Lit 2) (Lit 3)) (Lit 4)) `shouldBe` 20
+
+    it "should evaluate parsed expressions" $ do
+      evalStr "2+3" `shouldBe` Just 5
+      evalStr "2*3" `shouldBe` Just 6
+      evalStr "(2+3)*4" `shouldBe` Just 20
+      evalStr "2+3 *4" `shouldBe` Just 14
+      evalStr "2+3*" `shouldBe` Nothing
