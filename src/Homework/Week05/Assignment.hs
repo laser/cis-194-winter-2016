fdeb58535139b1e6fda5b10c77cf26b1e7f5f625
Mark Williams <markcpa@pacbell.net>
2016-01-23 16:28:27 -0800

    week 5 #4

--- src/Homework/Week05/Assignment.hs
+++ src/Homework/Week05/Assignment.hs
@@ -2,7 +2,9 @@ module Homework.Week05.Assignment (
   eval,
   evalStr,
   ExprT(..),
-  Expr(..)
+  Expr(..),
+  MinMax(..),
+  Mod7(..)
 ) where
 
 import Homework.Week05.ExprT
@@ -33,7 +35,28 @@ instance Expr ExprT where
     mul e1 e2 = Mul e1 e2
 
 -- #4
--- instance Integer Expr where
-   -- lit a = Lit a
-   --add = ???
-   --mul = ???
+instance Expr Integer where
+   lit e = e
+   add e1 e2 = e1 + e2
+   mul e1 e2 = e1 * e2
+
+instance Expr Bool where
+   lit n = if n <=0 then False else True
+   add e1 e2 = e1 || e2
+   mul e1 e2 = e1 && e2
+
+newtype MinMax = MinMax Integer deriving (Eq,Ord,Show)
+
+instance Expr MinMax where
+   lit n = MinMax n
+   add e1 e2 = max e1 e2
+   mul e1 e2 = min e1 e2
+
+newtype Mod7 = Mod7 Integer deriving (Eq,Show)
+
+instance Expr Mod7 where
+   lit n = Mod7 (mod n 7)
+   add (Mod7 n1) (Mod7 n2) = Mod7 (mod (n1 + n2) 7)
+   mul (Mod7 n1) (Mod7 n2) = Mod7 (mod (n1 * n2) 7)
+
+
--- test/Homework/Week05Spec.hs
+++ test/Homework/Week05Spec.hs
@@ -28,3 +28,32 @@ spec = do
     it "should evaluate Expr to ExprT" $ do
       (lit 2) `shouldBe` (Lit 2)
       mul (add (lit 2) (lit 3)) (lit 4) `shouldBe` (Mul (Add (Lit 2) (Lit 3)) (Lit 4))
+
+    it "should treat Integers as instances of Expr" $ do
+      -- have to cast to Integer ... hmm
+      (lit 2) `shouldBe` (toInteger 2)
+      (add 2 3) `shouldBe` (toInteger 5)
+      (add 2 (mul 2 3)) `shouldBe` (toInteger 8)
+
+    it "should treat Bools as instances of Expr" $ do
+      (lit 2) `shouldBe` True
+      (lit 1) `shouldBe` True
+      (lit 0) `shouldBe` False
+      (lit (-1)) `shouldBe` False
+      (add (lit 0) (lit 2)) `shouldBe` True
+      (add (lit 0) (lit 0)) `shouldBe` False
+      (mul (lit 1) (lit 2)) `shouldBe` True
+      (mul (lit 1) (lit 0)) `shouldBe` False
+
+    it "should treat MinMax as instances of Expr" $ do
+      (lit 2) `shouldBe` MinMax 2
+      (add (lit 0) (lit 2)) `shouldBe` MinMax 2
+      (mul (lit 0) (lit 2)) `shouldBe` MinMax 0
+
+    it "should treat Mod7 as instances of Expr" $ do
+      (lit 7) `shouldBe` Mod7 0
+      (lit 3) `shouldBe` Mod7 3
+      (lit 9) `shouldBe` Mod7 2
+      (add (lit 5) (lit 3)) `shouldBe` Mod7 1
+      (mul (lit 5) (lit 4)) `shouldBe` Mod7 6
+      (add (lit 1)(mul (lit 4)(lit 5))) `shouldBe` Mod7 0
