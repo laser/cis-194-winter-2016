2d5cb4d8aa99afda52eeeea9e0fa48c70ed6e1d3
zhuochengyu <zyu@cj.com>
2016-01-23 23:26:29 -0800

    week03 tests passing but #3 solution is too long

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -4,15 +4,15 @@ module Homework.Week03.Assignment (
   histogram
 ) where
 
-getTheRightStringBaseOnTheStartingPoint :: [(a,Int)] -> Int -> [a]
-getTheRightStringBaseOnTheStartingPoint listWithIndices startingPoint
+getTheRightElementBaseOnTheStartingPoint :: [(a,Int)] -> Int -> [a]
+getTheRightElementBaseOnTheStartingPoint listWithIndices startingPoint
   = map fst (filter (\x -> mod (snd x) startingPoint == 0) listWithIndices )
 
 -- #1
 skips :: [a] -> [[a]]
 skips [] = []
 skips list
-  = map (getTheRightStringBaseOnTheStartingPoint (zip list [1..])) [1 .. (length list)]
+  = map (getTheRightElementBaseOnTheStartingPoint (zip list [1..])) [1 .. (length list)]
 
 -- #2
 localMaxima :: [Integer] -> [Integer]
@@ -22,5 +22,32 @@ localMaxima (x:y:z:xs) = case y > x && y > z of
 localMaxima doesntmatch = []
 
 -- #3
+getCountList :: [Integer] -> [Int]
+getCountList list = [length $ filter (==0) list
+                    ,length $ filter (==1) list
+                    ,length $ filter (==2) list
+                    ,length $ filter (==3) list
+                    ,length $ filter (==4) list
+                    ,length $ filter (==5) list
+                    ,length $ filter (==6) list
+                    ,length $ filter (==7) list
+                    ,length $ filter (==8) list
+                    ,length $ filter (==9) list]
+
+getStarOrSpace :: Int -> Char
+getStarOrSpace 0 = ' '
+getStarOrSpace _ = '*'
+
+decrementByOneIfInputIsGreaterThanZero :: Int -> Int
+decrementByOneIfInputIsGreaterThanZero 0 = 0
+decrementByOneIfInputIsGreaterThanZero x = x - 1
+
+
+getHistogramWithCounts :: [Int] -> String
+getHistogramWithCounts list = case list of
+                                [0,0,0,0,0,0,0,0,0,0] -> ""
+                                otherwise -> getHistogramWithCounts (map decrementByOneIfInputIsGreaterThanZero list) ++ map getStarOrSpace list ++ "\n"
+
+
 histogram :: [Integer] -> String
-histogram = undefined
+histogram list = getHistogramWithCounts (getCountList list) ++ "==========\n0123456789\n"
