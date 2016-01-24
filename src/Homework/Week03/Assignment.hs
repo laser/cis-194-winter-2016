aa4c0ebce2cfd49c676cac6c9d66ec479d4cf212
Nicolas Font <nfont@cj.com>
2016-01-23 17:05:45 -0800

    skips

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -4,9 +4,20 @@ module Homework.Week03.Assignment (
   histogram
 ) where
 
+import Data.List
+
 -- #1
 skips :: [a] -> [[a]]
-skips = undefined
+skips [] = []
+skips xs = skips' 0 xs
+
+skips' :: Int -> [a] -> [[a]]
+skips' _ [] = []
+skips' n xs = (skipsEvery n xs) : skips' (n + 1) (drop 1 xs)
+
+skipsEvery :: Int -> [a] -> [a]
+skipsEvery _ [] = []
+skipsEvery n (x:xs) = x : skipsEvery n (drop n xs)
 
 -- #2
 localMaxima :: [Integer] -> [Integer]
