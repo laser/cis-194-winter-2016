d169119125a0686a91ae5424788aa13f2545df48
Vargas <jvargas@WW-JVARGAS-MAC.local>
2016-01-24 20:00:17 -0800

    Add week03 assignment

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -6,12 +6,25 @@ module Homework.Week03.Assignment (
 
 -- #1
 skips :: [a] -> [[a]]
-skips = undefined
+skips xs = map (\n -> skip n 0 xs) [0..length xs - 1]
+  where
+    skip _ _ [] = []
+    skip goal accum (y:ys)
+      | accum == goal = y : skip goal 0 ys
+      | accum < goal  = skip goal (accum + 1) ys
+      | otherwise = error "Bad arg value: 'accum' should never be greater than 'goal'"
 
 -- #2
 localMaxima :: [Integer] -> [Integer]
-localMaxima = undefined
+localMaxima xs@(a:b:c:_) = (if b > a && b > c then [b] else []) ++ localMaxima (tail xs)
+localMaxima (_:xs) = localMaxima xs
+localMaxima [] = []
 
 -- #3
 histogram :: [Integer] -> String
-histogram = undefined
+histogram xs = let
+  increment y ys = take y ys ++ [ys !! y + 1] ++ drop (y + 1) ys
+  counts = foldr increment (replicate 10 0) (map fromIntegral xs)
+  rows = foldr1 max counts
+  mkRow row = map (\col -> if counts !! col >= row then '*' else ' ') [0..9] ++ "\n"
+  in concat (map mkRow [rows,rows-1..1]) ++ "==========\n0123456789\n"
