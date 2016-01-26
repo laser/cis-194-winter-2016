f56c85dc1bfb5f30f76092a354e67fd0f37a5dbe
Abdussalam Abdurrahman <abdusalam@live.com>
2016-01-26 13:31:11 -0800

    week 3, exercise 1

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -6,7 +6,20 @@ module Homework.Week03.Assignment (
 
 -- #1
 skips :: [a] -> [[a]]
-skips = undefined
+skips = reverse . foldr step [] . reverse . tails'
+    where step xs accu = (take1stAndEveryNth n xs) : accu
+            where n = length accu + 1
+
+-- Data.List.tails sans an [] at the end
+tails' :: [a] -> [[a]]
+tails' xs@(_ : ys) = xs : tails' ys
+tails' _           = []
+
+--  n > 0
+take1stAndEveryNth :: Int -> [a] -> [a]
+take1stAndEveryNth _ []       = []
+take1stAndEveryNth n (x : xs) = x : take1stAndEveryNth n ys
+    where ys = drop (n - 1) xs
 
 -- #2
 localMaxima :: [Integer] -> [Integer]
