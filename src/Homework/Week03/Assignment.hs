c6168cf3d77d8c16eb557d5834859596fe1c945a
jeegarshah <jeegar@hotmail.com>
2016-01-25 21:54:24 -0800

    working skips with sub functions

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -6,8 +6,10 @@ module Homework.Week03.Assignment (
 
 -- #1
 skips :: [a] -> [[a]]
-skips = undefined
-
+skips xs = map snd (map unzip (map  (loopNumberOFElementsTimes zipWithIdx) zipWithIdx))
+    where zipWithIdx = zip [1..] xs
+          loopNumberOFElementsTimes zipWithIdx (loopIdx, _) = filter (indexMultipleOf $ loopIdx) zipWithIdx
+          indexMultipleOf multiple (idx, element) =  idx `mod` multiple == 0
 -- #2
 localMaxima :: [Integer] -> [Integer]
 localMaxima = undefined
