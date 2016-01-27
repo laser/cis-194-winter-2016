b82772a07b5c9294cd57950b8ea0cb42b9c1ff1f
Nicolas Font <nfont@cj.com>
2016-01-27 00:08:25 -0800

    histogram

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -13,17 +13,36 @@ skips xs = skips' 0 xs
 
 skips' :: Int -> [a] -> [[a]]
 skips' _ [] = []
-skips' n xs = (skipsEvery n xs) : skips' (n + 1) (drop 1 xs)
+skips' n xs = skipsEvery n xs : skips' (n + 1) (tail xs)
 
 skipsEvery :: Int -> [a] -> [a]
 skipsEvery _ [] = []
 skipsEvery n (x:xs) = x : skipsEvery n (drop n xs)
 
 -- #2
-localMaxima :: [Integer] -> [Integer]
+localMaxima :: [Int] -> [Int]
 localMaxima (a:b:c:xs) = (if (a < b && c < b) then [b] else []) ++ localMaxima (b:c:xs)
 localMaxima _ = []
 
 -- #3
-histogram :: [Integer] -> String
-histogram = undefined
+histogram :: [Int] -> String
+histogram xs = unlines $ histogramRows xs ++ ["==========", "0123456789"]
+
+histogramRows :: [Int] -> [String]
+histogramRows [] = []
+histogramRows ints = histogramRows' (maximum counts) counts
+  where counts = histogramCounts ints
+
+histogramCounts :: [Int] -> [Int]
+histogramCounts ints = map (\ i -> frequency i ints) [0..9]
+
+frequency :: Int -> [Int] -> Int
+frequency _ [] = 0
+frequency i ints = frequency i (tail ints) + if (i == (head ints)) then 1 else 0
+
+histogramRows' :: Int -> [Int] -> [String]
+histogramRows' 0 _ = []
+histogramRows' n ints = histogramRow n ints : histogramRows' (n - 1) ints
+
+histogramRow :: Int -> [Int] -> String
+histogramRow n counts = map (\ c -> if (c >= n) then '*' else ' ') counts
