09b923c9dd4b82fd0681c429a083acce49399602
Alexis King <lexi.lambda@gmail.com>
2016-01-26 22:53:31 -0800

    Add week 4

--- src/Homework/Week04/Assignment.hs
+++ src/Homework/Week04/Assignment.hs
@@ -19,72 +19,103 @@ module Homework.Week04.Assignment (
   BST(..)
 ) where
 
+import Data.Char  (isSpace, isUpper)
+import Data.List  (intercalate)
+import Data.Maybe (mapMaybe)
+
 import Homework.Week04.BST
 
 -- #1
+-- only one implementation (basically just id, but throws away the first arg)
 ex1 :: a -> b -> b
-ex1 = undefined
+ex1 _ = id
 
 -- #2
+-- two possible implementations, could use the first or second arg
 ex2 :: a -> a -> a
-ex2 = undefined
+ex2 x _ = x
 
 -- #3
+-- still only one implementation, since we still only have exactly one “a”
 ex3 :: Int -> a -> a
-ex3 = undefined
+ex3 _ = id
 
 -- #4
+-- precisely four possible implementations:
+--   - always return arg 2
+--   - always return arg 3
+--   - return arg 2 if arg 1 is True, otherwise arg 3
+--   - return arg 3 if arg 1 is True, otherwise arg 2
 ex4 :: Bool -> a -> a -> a
-ex4 = undefined
+ex4 c a b = if c then a else b
 
 -- #5
+-- two possible implementations, either id or not
 ex5 :: Bool -> Bool
-ex5 = undefined
+ex5 = not
 
 -- #6
+-- impossible because we still need an “a” to get an “a”
 ex6 :: (a -> a) -> a
-ex6 = undefined
+ex6 = error "impossible"
 
 -- #7
+-- infinite possible implementations: could thread the second arg through the
+-- first arg any number of times
 ex7 :: (a -> a) -> a -> a
-ex7 = undefined
+ex7 = ($)
 
 -- #8
+-- infinite possible implementations since the source can be used to create
+-- arbitrary sublists, but the empty array can only return the empty array
 ex8 :: [a] -> [a]
-ex8 = undefined
+ex8 xs = take (length xs `div` 2) xs
 
 -- #9
+-- infinite possible implementations for the same reason as #8, but actually
+-- getting any values requires effectively mapping
 ex9 :: (a -> b) -> [a] -> [b]
-ex9 = undefined
+ex9 = map
 
 -- #10
+-- impossible to implement due to the Nothing case
 ex10 :: Maybe a -> a
-ex10 = undefined
+ex10 = error "impossible"
 
 -- #11
+-- two possible implementations: Just or (const Nothing)
 ex11 :: a -> Maybe a
-ex11 = undefined
+ex11 = Just
 
 -- #12
+-- two possible implementations: id or (const Nothing)
 ex12 :: Maybe a -> Maybe a
-ex12 = undefined
+ex12 = id
 
 -- #13
 insertBST :: (a -> a -> Ordering) -> a -> BST a -> BST a
-insertBST = undefined
+insertBST _ x Leaf = Node Leaf x Leaf
+insertBST cmp x (Node left y right)
+  | x `cmp` y == LT = Node (insertBST cmp x left) y right
+  | otherwise       = Node left y (insertBST cmp x right)
+
+safeHead :: [a] -> Maybe a
+safeHead []    = Nothing
+safeHead (x:_) = Just x
 
 -- #14
 allCaps :: [String] -> Bool
-allCaps = undefined
+allCaps = all isCapitalized
+  where isCapitalized = maybe False isUpper . safeHead
 
 -- #15
 dropTrailingWhitespace :: String -> String
-dropTrailingWhitespace = undefined
+dropTrailingWhitespace = reverse . dropWhile isSpace . reverse
 
 -- #16
 firstLetters :: [String] -> [Char]
-firstLetters = undefined
+firstLetters = mapMaybe safeHead
 
 -- #17
 asList :: [String] -> String
-asList = undefined
+asList str = "[" ++ intercalate "," str ++ "]"
