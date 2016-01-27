697890c405be4b560f3ec3a21376c36fc58cd112
Alexis King <lexi.lambda@gmail.com>
2016-01-27 12:58:58 -0800

    Add week 5 (without exercise 5/6)

--- src/Homework/Week05/Assignment.hs
+++ src/Homework/Week05/Assignment.hs
@@ -1,7 +1,10 @@
 module Homework.Week05.Assignment (
   eval,
   evalStr,
-  ExprT(..)
+  Expr(..),
+  ExprT(..),
+  MinMax(..),
+  Mod7(..)
 ) where
 
 import Homework.Week05.ExprT
@@ -9,20 +12,45 @@ import Homework.Week05.Parser
 
 -- #1
 eval :: ExprT -> Integer
-eval = undefined
+eval (Lit x)   = x
+eval (Add a b) = eval a + eval b
+eval (Mul a b) = eval a * eval b
 
 -- #2
 evalStr :: String -> Maybe Integer
-evalStr = undefined
+evalStr = fmap eval . parseExp Lit Add Mul
 
 -- #3
--- class Expr a where 
---   lit :: ???
---   add :: ???
---   mul :: ???
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
 
 -- #4
--- instance Expr Integer where
---   lit = ???
---   add = ???
---   mul = ???
+newtype MinMax = MinMax Integer deriving (Eq, Ord, Show)
+newtype Mod7   = Mod7   Integer deriving (Eq, Show)
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit = (> 0)
+  add = (||)
+  mul = (&&)
+
+instance Expr MinMax where
+  lit = MinMax
+  add = max
+  mul = min
+
+instance Expr Mod7 where
+  lit                   = Mod7 . (`mod` 7)
+  add (Mod7 a) (Mod7 b) = Mod7 $ (a + b) `mod` 7
+  mul (Mod7 a) (Mod7 b) = Mod7 $ (a * b) `mod` 7
