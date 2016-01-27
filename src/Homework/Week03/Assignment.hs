239312ba9f4cf9b2e9e7d3b87094ecf1fbd43452
Erin Swenson-Healey <eswenson-healey@cj.com>
2016-01-26 20:31:51 -0800

    Week 3 impl

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -4,14 +4,26 @@ module Homework.Week03.Assignment (
   histogram
 ) where
 
+import           Data.List (find, sort, span)
+
 -- #1
 skips :: [a] -> [[a]]
-skips = undefined
+skips xs = map (\n -> foldr (\(m, y) a -> if m `mod` n == 0 then y : a else a) [] $ zip [1..] xs) [1..length xs]
 
 -- #2
 localMaxima :: [Integer] -> [Integer]
-localMaxima = undefined
+localMaxima (i:j:k:ks) = let z = localMaxima (j:k:ks) in
+                         if (i < j && k < j) then j:z else z
+localMaxima _ = []
 
 -- #3
 histogram :: [Integer] -> String
-histogram = undefined
+histogram = (++"==========\n0123456789\n") . rows . df
+  where
+    dot n = if n > 0 then '*' else ' '
+    rows ns = maybe "" (const $ rows (map (+(-1)) ns) ++ (map dot ns ++ "\n")) (find (>0) ns)
+    df = go 0 . sort
+      where go n ns
+              | n == 10 = []
+              | 1>0 = let (l,r) = span (==n) ns in
+                      length l:go (n+1) r
