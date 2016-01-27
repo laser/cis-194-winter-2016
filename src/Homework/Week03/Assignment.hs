4d67ece152e1e9541d981780cc5353314ed818ac
jeegarshah <jeegar@hotmail.com>
2016-01-27 10:44:38 -0800

    refactoring2

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -22,10 +22,9 @@ localMaxima1 _ = []
 
 localMaxima2 :: [Integer] -> [Integer]
 localMaxima2 xs = map (\(_:x2:_) -> x2) (filter (\x -> case x of
-                                                           x1:[] -> False
-                                                           x1:x2:[] -> False
-                                                           x1:x2:x3:[] -> (x1 < x2 && x2 > x3)) (group3 xs))
---localMaxima xs = filter (\_ -> True ) (group3 xs)
+                                                            x1:x2:x3:[] -> (x1 < x2 && x2 > x3)
+                                                            _ -> False
+                                                           ) (group3 xs))
 
 group3 :: [a] -> [[a]]
 group3 xs = map snd (map unzip (map (\(skipPosition,_) -> filter(\(p, e) -> (skipPosition-p == -1) || (skipPosition-p == 0) || (skipPosition-p == 1)) zipWithPosition) zipWithPosition))
@@ -33,29 +32,18 @@ group3 xs = map snd (map unzip (map (\(skipPosition,_) -> filter(\(p, e) -> (ski
 
 -- #3
 histogram :: [Integer] -> String
-histogram xs = printStr . printLine $ xs
+histogram = toString . printLines
 
---filterNum :: Integer -> Integer -> Bool
---filterNum radix num = num == radix
+toString :: [[String]] -> String
+toString xs = unlines ((printLineString xs) ++ [(replicate 10 '=')] ++ [concat $ map show [0..9]])
+    where   printLineString xs = cleanLineString ( map concat xs)
+            cleanLineString xs = dropWhile (\x -> words x == []) xs
 
-countFor :: Integer -> [Integer] -> Int
-countFor r xs = length . filter (==r) $ xs
+printLines :: [Integer] -> [[String]]
+printLines xs = map(\row -> printRow row . countAll $ xs)[9,8..1]
+    where   printRow row counts = map (\count -> if(count-row > -1) then "*" else " ") counts
+            countAll xs         = map (\x -> countFor x xs)[0..9]
+            countFor r xs       = length . filter (==r) $ xs
 
-count0to9 :: [Integer] -> [Int]
-count0to9 xs = map (\x -> countFor x xs)[0..9]
 
-printFor :: Int -> [Int] -> [String]
-printFor pos xs = map (\count -> if(count-pos > -1) then "*" else " ") xs 
-
-printLine :: [Integer] -> [[String]]
-printLine xs = map(\x -> printFor x (count0to9 xs))[9,8..1]
-
-printLineString :: [[String]] -> [String]
-printLineString xs = cleanLineString ( map concat xs)
-
-cleanLineString :: [String] -> [String]
-cleanLineString xs = dropWhile (\x -> words x == []) xs 
-
-printStr :: [[String]] -> String
-printStr xs = unlines ((printLineString xs) ++ [(replicate 10 '=')] ++ [concat $ map show [0..9]])
 
