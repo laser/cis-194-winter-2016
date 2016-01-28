83f583aac4e518b2c90cfce3d47fb8bb764714e0
Hitesh Lad <hlad@cj.com>
2016-01-27 17:36:07 -0800

    week 3 - problem 1 initial work

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -5,8 +5,17 @@ module Homework.Week03.Assignment (
 ) where
 
 -- #1
+every :: Int -> [a] -> [a]
+every n xs = case drop (n-1) xs of
+              (y:ys) -> y : every n ys
+              [] -> []
+
+takeIt :: Int -> [ a ] -> Int -> [[ a ]]
+takeIt _ _ 0  = []
+takeIt n xs todo = (every n xs) : (takeIt (n+1) (xs) (todo-1))
+
 skips :: [a] -> [[a]]
-skips = undefined
+skips xs = takeIt 1 xs (length xs)
 
 -- #2
 localMaxima :: [Integer] -> [Integer]
