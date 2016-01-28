df326fb5c849f554d646f1df09debaebb00fef95
Adrian Arcilla <aarcilla@cj.com>
2016-01-27 23:29:58 -0800

    Homework3: Tests for first problem pass

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -4,9 +4,30 @@ module Homework.Week03.Assignment (
   histogram
 ) where
 
+dropEveryOtherN :: Int -> [a] -> [a]
+dropEveryOtherN 0 p = p 
+dropEveryOtherN n [] = []
+dropEveryOtherN n (x : xs) = [x] ++ dropEveryOtherN n (drop n xs)
+
+stripHead :: [a] -> [[a]]
+stripHead [] = []
+stripHead p@(x:xs)
+	| length xs == 0 = [p]
+	| otherwise = [p] ++ stripHead xs
+
+zipFunctions :: [[a]] -> [(Int, [a])]
+zipFunctions p@(x:xs)= zip [0..((length p) - 1)] p 
+
+
+buildList :: [(Int, [a])] -> [[a]]
+buildList p@(x:xs)
+	| length xs == 0  = [dropEveryOtherN (fst x) (snd x)]
+	| otherwise = [dropEveryOtherN (fst x) (snd x)] ++ buildList xs
+
 -- #1
 skips :: [a] -> [[a]]
-skips = undefined
+skips [] = []
+skips p = buildList (zipFunctions (stripHead p))
 
 -- #2
 localMaxima :: [Integer] -> [Integer]
