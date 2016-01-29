a9c522b610bf7e53af8f9d2a24d30abc224c4586
Alexis King <lexi.lambda@gmail.com>
2016-01-28 19:05:27 -0800

    Add week 6

--- src/Homework/Week06/Assignment.hs
+++ src/Homework/Week06/Assignment.hs
@@ -11,39 +11,49 @@ module Homework.Week06.Assignment (
   Stream(..)
 ) where
 
+import Data.List (intercalate)
+
 -- #1a
 fib :: Integer -> Integer
-fib = undefined
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 2) + fib (n - 1)
 
 fibs1 :: [Integer]
-fibs1 = undefined
+fibs1 = map fib [0..]
 
 -- #2
 fibs2 :: [Integer]
-fibs2 = undefined
+fibs2 = 0 : 1 : zipWith (+) fibs2 (tail fibs2)
 
 -- #3
-data Stream a = Stream a -- replace this with your own definition; this one is wrong
+data Stream a = Cons a (Stream a)
 
 streamToList :: Stream a -> [a]
-streamToList = undefined
+streamToList (Cons x xs) = x : streamToList xs
 
--- instance Show a => Show (Stream a) where
---   show = ???
+instance Show a => Show (Stream a) where
+  show s = "[" ++ intercalate ", " strs ++ ", ..."
+    where xs   = take 20 (streamToList s)
+          strs = map show xs
 
 -- #4
 streamRepeat :: a -> Stream a
-streamRepeat = undefined
+streamRepeat x = Cons x (streamRepeat x)
 
 streamMap :: (a -> b) -> Stream a -> Stream b
-streamMap = undefined
+streamMap f (Cons x xs) = Cons (f x) (streamMap f xs)
 
 streamFromSeed :: (a -> a) -> a -> Stream a
-streamFromSeed = undefined
+streamFromSeed f acc = Cons acc (streamFromSeed f (f acc))
 
 -- #5
 nats :: Stream Integer
-nats = undefined
+nats = streamFromSeed (+1) 0
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons x xs) ys = Cons x (interleaveStreams ys xs)
 
 ruler :: Stream Integer
-ruler = undefined
+ruler = foldr1 interleaveStreams repeatedNats
+  where repeatedNats = map streamRepeat [0..]
