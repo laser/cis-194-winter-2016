2ddb6c6a2b4dfe174b0f5add5d0355c5afd8ad46
Alexis King <lexi.lambda@gmail.com>
2016-01-29 07:02:56 -0800

    Add week 7 implementation + a few tests

--- src/Homework/Week07/Assignment.hs
+++ src/Homework/Week07/Assignment.hs
@@ -8,72 +8,106 @@ module Homework.Week07.Assignment (
   search,
   firstFound,
   lastFound,
+  allFound,
   numberFound,
   orderedNtoS,
   Market(..),
   OrdList(..),
-  Searcher(..)
+  Searcher
 ) where
 
-import Data.Aeson
+import Control.Monad (join)
+import Data.Aeson    hiding ((.:))
 import Data.Monoid
 import GHC.Generics
 
 import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.Text as T
-import qualified Data.Text.IO as T
 
 -- #1
 ynToBool :: Value -> Value
-ynToBool = undefined
+ynToBool (String "Y") = Bool True
+ynToBool (String "N") = Bool False
+ynToBool (Array vs)   = Array $ fmap ynToBool vs
+ynToBool (Object vs)  = Object $ fmap ynToBool vs
+ynToBool other        = other
 
 -- #2
 parseData :: B.ByteString -> Either String Value
-parseData = undefined
+parseData = fmap ynToBool . eitherDecode
 
 -- #3
-data Market = Market { marketname :: String
+data Market = Market { marketname :: T.Text
                      , x :: Double
                      , y :: Double
-                     , state :: String } deriving (Show, Generic)
+                     , state :: T.Text
+                     } deriving (Eq, Show, Generic)
 
 instance FromJSON Market
 
+resultToEither :: Result a -> Either String a
+resultToEither (Success a) = Right a
+resultToEither (Error str) = Left str
+
 parseMarkets :: B.ByteString -> Either String [Market]
-parseMarkets = undefined
+parseMarkets = join . fmap (resultToEither . fromJSON) . parseData
 
 -- #4
+fromEither :: (a -> c) -> (b -> c) -> Either a b -> c
+fromEither f _ (Left l)  = f l
+fromEither _ f (Right r) = f r
+
 loadData :: IO [Market]
-loadData = undefined
+loadData = eitherToIO . parseMarkets =<< B.readFile "markets.json"
+  where eitherToIO = fromEither fail return
 
 -- #5
-data OrdList a = OrdList { getOrdList :: [a] } deriving (Eq, Show)
+data OrdList a = OrdList { getOrdList :: [a] }
+  deriving (Eq, Show)
 
---instance Ord a => Monoid (OrdList a) where
---  compare = ???
+instance Ord a => Monoid (OrdList a) where
+  mempty = OrdList []
+  mappend xs (OrdList []) = xs
+  mappend (OrdList []) ys = ys
+  mappend a@(OrdList (m:ms)) b@(OrdList (n:ns))
+    | m < n     = OrdList $ m : getOrdList (OrdList ms <> b)
+    | otherwise = OrdList $ n : getOrdList (a <> OrdList ns)
 
 -- #6
 type Searcher m = T.Text -> [Market] -> m
 
+infixr 8 .:
+(.:) :: (a -> b) -> (c -> d -> a) -> c -> d -> b
+(.:) f g a b = f (g a b)
+
+simpleSearch :: Searcher [Market]
+simpleSearch txt = filter (T.isInfixOf txt . marketname)
+
 search :: Monoid m => (Market -> m) -> Searcher m
-search = undefined
+search f = mconcat . map f .: simpleSearch
 
 -- #7
 firstFound :: Searcher (Maybe Market)
-firstFound = undefined
+firstFound = getFirst .: search (First . Just)
 
 -- #8
 lastFound :: Searcher (Maybe Market)
-lastFound = undefined
+lastFound = getLast .: search (Last . Just)
 
 -- #9
 allFound :: Searcher [Market]
-allFound = undefined
+allFound = simpleSearch
 
 -- #10
 numberFound :: Searcher Int
-numberFound = undefined
+numberFound = getSum .: search (Sum . const 1)
 
 -- #11
+newtype NtoS = NtoS { getNtoS :: Market }
+  deriving (Eq, Show)
+
+instance Ord NtoS where
+  compare (NtoS Market{y = a}) (NtoS Market{y = b}) = compare a b
+
 orderedNtoS :: Searcher [Market]
-orderedNtoS = undefined
+orderedNtoS = map getNtoS . getOrdList .: search (OrdList . (:[]) . NtoS)
--- test/Homework/Week07Spec.hs
+++ test/Homework/Week07Spec.hs
@@ -7,11 +7,79 @@ import Test.Hspec
 
 import Homework.Week07.Assignment
 
+import Data.Aeson
+import qualified Data.ByteString.Lazy.Char8 as B
+
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "week 7" $ do
-    it "needs some tests!" $ do
-      pending
+  describe "ynToBool" $ do
+    it "converts Y strings to True" $ do
+      ynToBool (toJSON "Y") `shouldBe` toJSON True
+
+    it "converts N strings to False" $ do
+      ynToBool (toJSON "N") `shouldBe` toJSON False
+
+    it "converts values nested within Arrays" $ do
+      ynToBool (toJSON ["Y", "N"]) `shouldBe` toJSON [True, False]
+
+    it "converts values nested within Objects" $ do
+      ynToBool (toJSON [("a", "Y"), ("b", "N")])
+        `shouldBe` toJSON [("a", True), ("b", False)]
+
+    it "lets other values pass through" $ do
+      ynToBool (toJSON "something") `shouldBe` toJSON "something"
+      ynToBool (toJSON True) `shouldBe` toJSON True
+      ynToBool (toJSON False) `shouldBe` toJSON False
+      ynToBool (toJSON (42 :: Double)) `shouldBe` toJSON (42 :: Double)
+      ynToBool (toJSON ["A", "B", "C"]) `shouldBe` toJSON ["A", "B", "C"]
+
+    it "handles deeply nested objects" $ do
+      {- Sorry for the mess: Haskell has no heredocs!
+         The input JSON looks like this:
+
+         {
+           "a": [{}, {"x": "Y"}],
+           "b": [{"y": 42}, {"z": ["A", "N"]}],
+         }
+
+         The "Y" and "N" should be replaced as expected. -}
+      let (Just input)  = decode' $ B.pack ("[{\"a\": [{}, {\"x\": \"Y\"}]}," ++
+                                            " {\"b\": [{\"y\": 42}," ++
+                                            " {\"z\": [\"A\", \"N\"]}]}]")
+      let (Just output) = decode' $ B.pack ("[{\"a\": [{}, {\"x\": true}]}," ++
+                                            " {\"b\": [{\"y\": 42}," ++
+                                            " {\"z\": [\"A\", false]}]}]")
+      ynToBool input `shouldBe` output
+
+  describe "parseData" $ do
+    it "returns an error for malformed JSON" $ do
+      parseData (B.pack "{") `shouldBe` Left "not enough input"
+
+    it "parses JSON strings, replacing Y/N with booleans" $ do
+      parseData (B.pack "\"Y\"") `shouldBe` Right (toJSON True)
+      parseData (B.pack "\"N\"") `shouldBe` Right (toJSON False)
+      parseData (B.pack "[1, 2, 3]")
+        `shouldBe` Right (toJSON ([1, 2, 3] :: [Double]))
+      parseData (B.pack "[\"N\", \"Y\", \"N\"]")
+        `shouldBe` Right (toJSON [False, True, False])
+
+  describe "Monoid OrdList" $ do
+    describe "mempty" $ do
+      it "is the empty list" $ do
+        mempty `shouldBe` OrdList ([] :: [Integer])
+
+    describe "mappend" $ do
+      it "preserves order in the result" $ do
+        (OrdList [1, 2, 3] `mappend` OrdList [4, 5, 6])
+          `shouldBe` OrdList ([1, 2, 3, 4, 5, 6] :: [Integer])
+        (OrdList [4, 5, 6] `mappend` OrdList [1, 2, 3])
+          `shouldBe` OrdList ([1, 2, 3, 4, 5, 6] :: [Integer])
+        (OrdList [1, 3, 6] `mappend` OrdList [2, 4, 5])
+          `shouldBe` OrdList ([1, 2, 3, 4, 5, 6] :: [Integer])
+        (OrdList [2, 4, 5] `mappend` OrdList [1, 3, 6])
+          `shouldBe` OrdList ([1, 2, 3, 4, 5, 6] :: [Integer])
+        (OrdList [1, 2, 3] `mappend` OrdList [1, 2, 3])
+          `shouldBe` OrdList ([1, 1, 2, 2, 3, 3] :: [Integer])
