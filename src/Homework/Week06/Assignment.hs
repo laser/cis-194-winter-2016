cd10fa314ce2b83f57d64b9079665ac8dc0790ea
mwilliams <mwilliams@cj.com>
2016-01-30 13:16:35 -0800

    week 6, but need a better ruler

--- src/Homework/Week06/Assignment.hs
+++ src/Homework/Week06/Assignment.hs
@@ -8,6 +8,7 @@ module Homework.Week06.Assignment (
   streamFromSeed,
   nats,
   ruler,
+  interleaveStreams,
   Stream(..)
 ) where
 
@@ -28,27 +29,50 @@ fibs2 = fibo 0 1
     
 
 -- #3
-data Stream a = Stream a -- replace this with your own definition; this one is wrong
+data Stream a = Cons a (Stream a)
 
 streamToList :: Stream a -> [a]
-streamToList = undefined
+streamToList (Cons a (s)) = a : streamToList s
 
--- instance Show a => Show (Stream a) where
---   show = ???
+instance Show a => Show (Stream a) where 
+  show = show . (take 20) . streamToList
 
 -- #4
 streamRepeat :: a -> Stream a
-streamRepeat = undefined
+streamRepeat a = Cons a (streamRepeat a)
 
 streamMap :: (a -> b) -> Stream a -> Stream b
-streamMap = undefined
+streamMap f (Cons a (s)) = Cons (f a) (streamMap f s)
 
 streamFromSeed :: (a -> a) -> a -> Stream a
-streamFromSeed = undefined
+streamFromSeed f a = Cons a (streamFromSeed f (f a))
 
 -- #5
 nats :: Stream Integer
-nats = undefined
+nats = streamFromSeed (+1) 0
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons a0 (s0))(Cons a1 (s1)) = Cons a0 (Cons a1 (interleaveStreams s0 s1))
 
 ruler :: Stream Integer
-ruler = undefined
+
+-- the divisibility way...
+ruler = interleaveStreams (streamRepeat 0) (streamMap highestPowerOf2ThatEvenlyDivides (streamFromSeed (+2) 2))
+    where highestPowerOf2ThatEvenlyDivides n = if mod n 2 == 0 
+                                               then 1 + highestPowerOf2ThatEvenlyDivides (div n 2) 
+                                               else 0
+
+-- the clever way, but how to create a recursive function that will evaluate lazily?
+-- we need something like this:
+--
+--ruler = interleaveStreams (streamRepeat 0)
+--            (interleaveStreams (streamRepeat 1)
+--                (interleaveStreams (streamRepeat 2)
+--                    (interleaveStreams (streamRepeat 3)
+--                        (interleaveStreams (streamRepeat 4)(streamRepeat 5)))))
+--
+--  ... but this never terminates...  :-(
+--
+--ruler = nestedInterleave 0
+--    where nestedInterleave n = interleaveStreams (streamRepeat n) (nestedInterleave $ (n + 1))
+
--- test/Homework/Week06Spec.hs
+++ test/Homework/Week06Spec.hs
@@ -27,3 +27,28 @@ spec = do
   describe "fibs2" $ do
     it "should calculate an infinte fibonacci sequence - efficiently" $ do
       last (take 35 fibs2) `shouldBe` 5702887
+
+  describe "streamRepeat" $ do
+    it "should produce a stream containing infinitely many copies of the same element" $ do
+      take 50 (streamToList (streamRepeat 1)) `shouldBe` (replicate 50 1)
+
+  describe "streamMap" $ do
+    it "should apply a function to every element of a stream" $ do
+      take 10 (streamToList (streamMap (*3) (streamRepeat 2))) `shouldBe` (replicate 10 6)
+
+  describe "streamFromSeed" $ do
+    it "should use the given function to generate the next element of a stream from a given element" $ do
+      take 10 (streamToList (streamFromSeed (*2) 2)) `shouldBe` [2,4,8,16,32,64,128,256,512,1024]
+
+  describe "nats" $ do
+    it "should be all natural numbers" $ do
+      take 20 (streamToList nats) `shouldBe` [0..19]
+
+  describe "interleaveStreams" $ do
+    it "should alternate values between two streams" $ do
+      take 6 (streamToList (interleaveStreams (streamRepeat 0) (streamRepeat 1))) `shouldBe` [0,1,0,1,0,1]
+
+  describe "ruler" $ do
+    it "should calc the largest power of 2 that evenly divides into each element (starting at 1)" $ do
+      take 32 (streamToList ruler) `shouldBe` [0,1,0,2,0,1,0,3,0,1,0,2,0,1,0,4,0,1,0,2,0,1,0,3,0,1,0,2,0,1,0,5]
+      
