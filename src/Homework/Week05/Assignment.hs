4f01837b6abf99999bf439cb5d3024468fa0c753
zhuochengyu <Gooki@zyu.fios-router.home>
2016-02-01 22:48:16 -0800

    finished week05

--- src/Homework/Week05/Assignment.hs
+++ src/Homework/Week05/Assignment.hs
@@ -2,14 +2,16 @@ module Homework.Week05.Assignment (
   eval,
   evalStr,
   ExprT(..),
-  -- uncomment these once you've defined them:
-  -- Expr(..),
-  -- MinMax(..),
-  -- Mod7(..)
+  Expr(..),
+  MinMax(..),
+  Mod7(..)
 ) where
 
 import Homework.Week05.ExprT
 import Homework.Week05.Parser
+
+newtype MinMax = MinMax Integer deriving(Eq, Show)
+newtype Mod7   = Mod7 Integer deriving(Eq, Show)
 
 -- #1
 eval :: ExprT -> Integer
@@ -24,13 +26,33 @@ evalStr input = case parseExp Lit Add Mul input of
   Nothing -> Nothing
 
 -- #3
--- class Expr a where
---   lit :: ???
---   add :: ???
---   mul :: ???
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
 
 -- #4
--- instance Expr Integer where
---   lit = ???
---   add = ???
---   mul = ???
+instance Expr Integer where
+  lit x = x
+  add a b = a + b
+  mul a b = a * b
+
+instance Expr Bool where
+  lit x = x > 0
+  add a b = a || b
+  mul a b = a && b
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax x) (MinMax y) = MinMax $ max x y
+  mul (MinMax x) (MinMax y) = MinMax $ min x y
+
+instance Expr Mod7 where
+  lit x = Mod7 (mod x 7)
+  add (Mod7 x) (Mod7 y) = Mod7 ((x + y) `mod` 7)
+  mul (Mod7 x) (Mod7 y) = Mod7 ((x * y) `mod` 7)
--- test/Homework/Week05Spec.hs
+++ test/Homework/Week05Spec.hs
@@ -34,63 +34,63 @@ spec = do
       evalStr "(2+3)*4" `shouldBe` Just 20
       evalStr "2+3*4" `shouldBe` Just 14
 
-  -- describe "Expr ExprT" $ do
-  --   it "produces expression ASTs" $ do
-  --     mul (add (lit 2) (lit 3)) (lit 4) `shouldBe`
-  --       Mul (Add (Lit 2) (Lit 3)) (Lit 4)
+  describe "Expr ExprT" $ do
+    it "produces expression ASTs" $ do
+      mul (add (lit 2) (lit 3)) (lit 4) `shouldBe`
+        Mul (Add (Lit 2) (Lit 3)) (Lit 4)
+
+  describe "Expr Integer" $ do
+    it "evaluates arithmetic expressions to integers" $ do
+      mul (add (lit 2) (lit 3)) (lit 4) `shouldBe` (20 :: Integer)
+
+  describe "Expr Bool" $ do
+    it "interprets negative values as False" $ do
+      property $ \(Positive x) -> lit (-x) `shouldBe` False
+
+    it "interprets zero as False" $ do
+      lit 0 `shouldBe` False
+
+    it "interprets positive values as True" $ do
+      property $ \(Positive x) -> lit x `shouldBe` True
+
+    it "evaluates addition as logical or" $ do
+      add (lit 0) (lit 0) `shouldBe` False
+      add (lit 1) (lit 0) `shouldBe` True
+      add (lit 0) (lit 1) `shouldBe` True
+      add (lit 1) (lit 1) `shouldBe` True
+
+    it "evaluates multiplication as logical and" $ do
+      mul (lit 0) (lit 0) `shouldBe` False
+      mul (lit 1) (lit 0) `shouldBe` False
+      mul (lit 0) (lit 1) `shouldBe` False
+      mul (lit 1) (lit 1) `shouldBe` True
+
+    it "evaluates arithmetic expressions as logical operations" $ do
+      mul (add (lit 2) (lit 3)) (lit 4) `shouldBe` True
+      mul (add (lit 2) (lit 3)) (lit 0) `shouldBe` False
+
+  describe "Expr MinMax" $ do
+    it "wraps literals in the MinMax newtype" $ do
+      property $ \x -> lit x `shouldBe` MinMax x
+
+    it "evaluates addition as maximum" $ do
+      property $ \x y -> add (lit x) (lit y) `shouldBe` MinMax (max x y)
+
+    it "evaluates multiplication as minimum" $ do
+      property $ \x y -> mul (lit x) (lit y) `shouldBe` MinMax (min x y)
+
+    it "evaluates arithmetic expressions as minimums/maximums" $ do
+      mul (add (lit 2) (lit 3)) (lit 4) `shouldBe` MinMax 3
   --
-  -- describe "Expr Integer" $ do
-  --   it "evaluates arithmetic expressions to integers" $ do
-  --     mul (add (lit 2) (lit 3)) (lit 4) `shouldBe` (20 :: Integer)
-  --
-  -- describe "Expr Bool" $ do
-  --   it "interprets negative values as False" $ do
-  --     property $ \(Positive x) -> lit (-x) `shouldBe` False
-  --
-  --   it "interprets zero as False" $ do
-  --     lit 0 `shouldBe` False
-  --
-  --   it "interprets positive values as True" $ do
-  --     property $ \(Positive x) -> lit x `shouldBe` True
-  --
-  --   it "evaluates addition as logical or" $ do
-  --     add (lit 0) (lit 0) `shouldBe` False
-  --     add (lit 1) (lit 0) `shouldBe` True
-  --     add (lit 0) (lit 1) `shouldBe` True
-  --     add (lit 1) (lit 1) `shouldBe` True
-  --
-  --   it "evaluates multiplication as logical and" $ do
-  --     mul (lit 0) (lit 0) `shouldBe` False
-  --     mul (lit 1) (lit 0) `shouldBe` False
-  --     mul (lit 0) (lit 1) `shouldBe` False
-  --     mul (lit 1) (lit 1) `shouldBe` True
-  --
-  --   it "evaluates arithmetic expressions as logical operations" $ do
-  --     mul (add (lit 2) (lit 3)) (lit 4) `shouldBe` True
-  --     mul (add (lit 2) (lit 3)) (lit 0) `shouldBe` False
-  --
-  -- describe "Expr MinMax" $ do
-  --   it "wraps literals in the MinMax newtype" $ do
-  --     property $ \x -> lit x `shouldBe` MinMax x
-  --
-  --   it "evaluates addition as maximum" $ do
-  --     property $ \x y -> add (lit x) (lit y) `shouldBe` MinMax (max x y)
-  --
-  --   it "evaluates multiplication as minimum" $ do
-  --     property $ \x y -> mul (lit x) (lit y) `shouldBe` MinMax (min x y)
-  --
-  --   it "evaluates arithmetic expressions as minimums/maximums" $ do
-  --     mul (add (lit 2) (lit 3)) (lit 4) `shouldBe` MinMax 3
-  --
-  -- describe "Expr Mod7" $ do
-  --   it "produces mod 7 literals" $ do
-  --     property $ \x -> lit x `shouldBe` Mod7 (x `mod` 7)
-  --
-  --   it "evaluates addition mod 7" $ do
-  --     property $ \x y -> add (lit x) (lit y) `shouldBe` Mod7 ((x + y) `mod` 7)
-  --
-  --   it "evaluates multiplication mod 7" $ do
-  --     property $ \x y -> mul (lit x) (lit y) `shouldBe` Mod7 ((x * y) `mod` 7)
-  --
-  --   it "evaluates arithmetic expressions to integers mod 7" $ do
-  --     mul (add (lit 2) (lit 3)) (lit 4) `shouldBe` Mod7 6
+  describe "Expr Mod7" $ do
+    it "produces mod 7 literals" $ do
+      property $ \x -> lit x `shouldBe` Mod7 (x `mod` 7)
+
+    it "evaluates addition mod 7" $ do
+      property $ \x y -> add (lit x) (lit y) `shouldBe` Mod7 ((x + y) `mod` 7)
+
+    it "evaluates multiplication mod 7" $ do
+      property $ \x y -> mul (lit x) (lit y) `shouldBe` Mod7 ((x * y) `mod` 7)
+
+    it "evaluates arithmetic expressions to integers mod 7" $ do
+      mul (add (lit 2) (lit 3)) (lit 4) `shouldBe` Mod7 6
