bb0371349482aff7711feb27c762e41a4ca5cd84
Hitesh Lad <hlad@cj.com>
2016-02-01 16:31:56 -0800

    week 4 - start homework 4, 1-14

--- src/Homework/Week04/Assignment.hs
+++ src/Homework/Week04/Assignment.hs
@@ -20,63 +20,101 @@ module Homework.Week04.Assignment (
 ) where
 
 import Homework.Week04.BST
+import Data.Char (isUpper)
 
 -- #1
 ex1 :: a -> b -> b
-ex1 = undefined
+-- always return the second argument
+ex1 x y = y
 
 -- #2
 ex2 :: a -> a -> a
-ex2 = undefined
+-- always return the first argument
+ex2 x y = x
 
 -- #3
 ex3 :: Int -> a -> a
-ex3 = undefined
+-- same as ex1, but the irst arg is an int
+ex3 = ex1
 
 -- #4
 ex4 :: Bool -> a -> a -> a
-ex4 = undefined
+-- my impl is a simple if
+-- five possible impls
+--  always second arg
+--  always third arg
+--  if first arg then second else third
+--  if first arg then third else second
+--  returns randomly second or third
+ex4 True y z = y
+ex4 False y z = z
 
 -- #5
 ex5 :: Bool -> Bool
-ex5 = undefined
+-- five possible impls
+--  1) always true
+--  2) always false
+--  3) returns input
+--  4) returns opposite of input (this is implemented below)
+--  5) returns random boolean
+ex5 x = not x
 
 -- #6
 ex6 :: (a -> a) -> a
-ex6 = undefined
+ex6 = error "impossible"
 
 -- #7
 ex7 :: (a -> a) -> a -> a
-ex7 = undefined
+-- two possible impls
+-- 1) return second arg
+-- 2) returns result of first arg execed with second arg
+ex7 fx x = fx x
 
 -- #8
 ex8 :: [a] -> [a]
-ex8 = undefined
+-- infinite number of args
+-- my impl reverses the list
+ex8 xs = reverse xs
 
 -- #9
 ex9 :: (a -> b) -> [a] -> [b]
-ex9 = undefined
+-- only solution is to map from list of a's to list of b's
+ex9 fx alist = map fx alist
 
 -- #10
 ex10 :: Maybe a -> a
-ex10 = undefined
+-- no total solution possible
+ex10 = error "impossible"
 
 -- #11
 ex11 :: a -> Maybe a
-ex11 = undefined
+-- two possible solutions
+-- 1) return Just a
+-- 2) always return Nothing
+ex11 _ = Nothing
 
 -- #12
 ex12 :: Maybe a -> Maybe a
-ex12 = undefined
+-- two possible solutions
+-- 1) return input
+-- 2) always return Nothing
+ex12 _ = Nothing
 
 -- #13
 insertBST :: (a -> a -> Ordering) -> a -> BST a -> BST a
-insertBST = undefined
+insertBST _ x Leaf                    = Node Leaf x Leaf
+insertBST ordering newval (Node left oldval right)
+       | ordering newval oldval == LT = Node (insertBST ordering newval left) oldval  right
+       | ordering newval oldval == GT = Node left oldval (insertBST ordering newval right)
+       | otherwise                    = Node left oldval right
 
 -- #14
-allCaps :: [String] -> Bool
-allCaps = undefined
+thisCap :: String -> Bool
+thisCap (x:ys) = isUpper x
+thisCap _ = False
 
+allCaps :: [String] -> Bool
+allCaps xs = all thisCap xs
 -- #15
 dropTrailingWhitespace :: String -> String
 dropTrailingWhitespace = undefined
