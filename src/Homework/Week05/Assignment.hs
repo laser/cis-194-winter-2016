aebc650173a937dffaf4a0c9a60d62d79603d0a7
jvargas <jvargas@cj.com>
2016-02-02 20:14:27 -0800

    doWeek05

--- src/Homework/Week05/Assignment.hs
+++ src/Homework/Week05/Assignment.hs
@@ -3,9 +3,9 @@ module Homework.Week05.Assignment (
   evalStr,
   ExprT(..),
   -- uncomment these once you've defined them:
-  -- Expr(..),
-  -- MinMax(..),
-  -- Mod7(..)
+  Expr(..),
+  MinMax(..),
+  Mod7(..)
 ) where
 
 import Homework.Week05.ExprT
@@ -13,20 +13,52 @@ import Homework.Week05.Parser
 
 -- #1
 eval :: ExprT -> Integer
-eval = undefined
+eval (Lit v) = v
+eval (Add a b) = eval a + eval b
+eval (Mul a b) = eval a * eval b
 
 -- #2
 evalStr :: String -> Maybe Integer
-evalStr = undefined
+evalStr = parseExp id (+) (*)
+
 
 -- #3
--- class Expr a where
---   lit :: ???
---   add :: ???
---   mul :: ???
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
 
 -- #4
--- instance Expr Integer where
---   lit = ???
---   add = ???
---   mul = ???
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit a = if a <= 0 then False else True
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer
+  deriving (Show, Eq)
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax a) (MinMax b) = MinMax (max a b)
+  mul (MinMax a) (MinMax b) = MinMax (min a b)
+
+newtype Mod7 = Mod7 Integer
+  deriving (Show, Eq)
+
+instance Expr Mod7 where
+  lit = Mod7 . mod7
+  add (Mod7 a) (Mod7 b) = Mod7 (mod7 $ a + b)
+  mul (Mod7 a) (Mod7 b) = Mod7 (mod7 $ a * b)
+
+mod7 :: Integer -> Integer
+mod7 = flip mod 7
--- test/Homework/Week05Spec.hs
+++ test/Homework/Week05Spec.hs
@@ -15,7 +15,6 @@ spec :: Spec
 spec = do
   describe "week 5 specs" $
     it "are commented out below" pending
-{-
   describe "eval" $ do
     it "evaluates Lit expressions as plain integers" $ do
       property $ \x -> eval (Lit x) `shouldBe` x
@@ -97,4 +96,3 @@ spec = do
 
     it "evaluates arithmetic expressions to integers mod 7" $ do
       mul (add (lit 2) (lit 3)) (lit 4) `shouldBe` Mod7 6
--}
