a525435623ce81de328324bee21ef0ddca7ad0c3
jvargas <jvargas@cj.com>
2016-02-03 09:13:35 -0800

    doWeek05NoPending

--- test/Homework/Week05Spec.hs
+++ test/Homework/Week05Spec.hs
@@ -14,7 +14,7 @@ main = hspec spec
 spec :: Spec
 spec = do
   describe "week 5 specs" $
-    it "are commented out below" pending
+    it "are commented out below"
   describe "eval" $ do
     it "evaluates Lit expressions as plain integers" $ do
       property $ \x -> eval (Lit x) `shouldBe` x
