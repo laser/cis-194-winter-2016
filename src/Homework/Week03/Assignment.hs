ebc500eccc835693ed6dc061c3ae81851230d56e
Bill Adams <badams@cj.com>
2016-02-04 05:54:44 -0800

    de-lintingw

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -17,35 +17,35 @@ skips xss = takeWhile (\x -> case x of
 
 
 skips' :: [a] -> [[a]]
-skips' xs  = map (\ x-> case length x of 
+skips' xs  = map (\ x-> case length x of
                            2 -> [head x]
                            3 -> [head x, last x]
-                           _ -> x ) $ go xs 
+                           _ -> x ) $ go xs
 go :: [a] -> [[a]]
 go [] = []
-go xs = xs : ( go $ tail xs)
+go xs = xs : go (tail xs)
 
--- #2 
+-- #2
 localMaxima :: [Integer] -> [Integer]
 localMaxima (a:b:c:xs)
   | a < b && b > c = b : localMaxima (c : xs)
-  | otherwise      = localMaxima (b : c : xs)                     
+  | otherwise      = localMaxima (b : c : xs)
 localMaxima _      = []
 
 scale :: Int
 scale = 10
 
 indices :: [Int]
-indices   = [0..(scale - 1)]  
+indices   = [0..(scale - 1)]
 
 histogram :: [Int] -> String
-histogram xs = makeLines ++ legend 
-               where makeLines  = loop "" $ theMap xs      
+histogram xs = makeLines ++ legend
+               where makeLines  = loop "" $ theMap xs
                      theMap     = Map.fromList . map (\x -> (head x, length x) ) . group . sort
-                     legend     = (replicate scale '=') ++ "\n" ++ (concatMap show indices) ++ "\n" 
+                     legend     = replicate scale '=' ++ "\n" ++ concatMap show indices ++ "\n"
 
-loop :: String -> Map.Map Int Int -> String 
+loop :: String -> Map.Map Int Int -> String
 loop theLines theMap = if (length theMap) == 0 then  theLines else loop (line ++ "\n" ++ theLines) restMxs
-               where line     = map starOrSpace $ map(\x -> Map.lookup x theMap) indices
+               where line     = map (starOrSpace . (`Map.lookup` theMap)) indices
                      restMxs  =  Map.filter(>0) $ Map.map pred theMap
-                     starOrSpace x = if (isJust x) then '*' else ' '  
+                     starOrSpace x = if isJust x then '*' else ' '
