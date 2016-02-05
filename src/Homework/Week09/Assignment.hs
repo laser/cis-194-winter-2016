4745986cff3cefde9f41007e044614966cf013a7
Alexis King <lexi.lambda@gmail.com>
2016-02-05 10:45:55 -0800

    Add week 9 + tests

--- src/Homework/Week09/Assignment.hs
+++ src/Homework/Week09/Assignment.hs
@@ -1,33 +1,91 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 module Homework.Week09.Assignment (
+  Atom(..),
+  SExpr(..),
   first,
   abParser,
   abParser_,
-  intOrUppercase
+  intPair,
+  intOrUppercase,
+  zeroOrMore,
+  oneOrMore,
+  spaces,
+  ident,
+  parseSExpr
 ) where
 
 import Homework.Week09.AParser
 
--- #1
-first :: (a -> b) -> (a,c) -> (b,c)
-first = undefined
+import qualified Control.Arrow       as Arr (first)
+import           Control.Applicative
+import           Control.Monad              (void)
+import           Data.Char                  (isAlpha, isAlphaNum, isSpace, isUpper)
 
--- #2
---instance Applicative Parser where
---  pure = ???
---  _ <*> _ = ???
+-- #1A
+first :: (a -> b) -> (a, c) -> (b, c)
+first = Arr.first
 
--- #3
+instance Functor Parser where
+  fmap f (Parser g) = Parser $ fmap (first f) . g
+
+-- #2A
+instance Applicative Parser where
+  pure a = Parser $ \s -> Just (a, s)
+
+  (Parser a) <*> (Parser b) = Parser $ \str -> do
+    (f, remainingA) <- a str
+    (x, remainingB) <- b remainingA
+    return (f x, remainingB)
+
+-- #3A
 abParser :: Parser (Char, Char)
-abParser = undefined
+abParser = (,) <$> char 'a' <*> char 'b'
 
 abParser_ :: Parser ()
-abParser_ = undefined
+abParser_ = void abParser
 
--- #4
---instance Alternative Parser where
---  empty = ???
---  _ <|> _ = ???
+intPair :: Parser [Integer]
+intPair = collect <$> posInt <* char ' ' <*> posInt
+  where collect a b = [a, b]
 
--- #5
+-- #4A
+instance Alternative Parser where
+  empty = Parser $ const Nothing
+  (Parser a) <|> (Parser b) = Parser $ \str -> a str <|> b str
+
+-- #5A
 intOrUppercase :: Parser ()
-intOrUppercase = undefined
+intOrUppercase = void posInt <|> void (satisfy isUpper)
+
+-- #1B
+zeroOrMore :: Parser a -> Parser [a]
+zeroOrMore p = oneOrMore p <|> pure []
+
+oneOrMore :: Parser a -> Parser [a]
+oneOrMore p = (:) <$> p <*> zeroOrMore p
+
+-- #2B
+spaces :: Parser String
+spaces = zeroOrMore $ satisfy isSpace
+
+ident :: Parser Ident
+ident = (:) <$> satisfy isAlpha <*> zeroOrMore (satisfy isAlphaNum)
+
+-- #3B
+type Ident = String
+
+data Atom = N Integer | I Ident
+  deriving (Eq, Show)
+
+data SExpr = A Atom | Comb [SExpr]
+  deriving (Eq, Show)
+
+parseAtom :: Parser Atom
+parseAtom =  N <$> posInt
+         <|> I <$> ident
+
+parseSExpr :: Parser SExpr
+parseSExpr = spaces *> inner <* spaces
+  where inner =  A    <$> parseAtom
+             <|> Comb <$> (char '(' *> zeroOrMore parseSExpr <* char ')')
--- test/Homework/Week09Spec.hs
+++ test/Homework/Week09Spec.hs
@@ -4,14 +4,126 @@ module Homework.Week09Spec (
 ) where
 
 import Test.Hspec
+import Test.QuickCheck
 
+import Homework.Week09.AParser
 import Homework.Week09.Assignment
+
+import Control.Applicative
+import Data.Char (isNumber, isUpper)
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
-  describe "week 9" $ do
-    it "needs some tests!" $ do
-      pending
+  describe "Functor Parser" $ do
+    describe "fmap" $ do
+      it "applies a function to the result of a parser" $ do
+        let p = fmap (+1) posInt
+        runParser p "41" `shouldBe` Just (42, "")
+        runParser p "x"  `shouldBe` Nothing
+
+  describe "Applicative Parser" $ do
+    describe "pure" $ do
+      it "creates a parser that consumes nothing and returns a value" $
+        property $ \str -> runParser (pure ()) str == Just ((), str)
+
+    describe "<*>" $ do
+      it "applies a function from a parser to the result of a parser" $ do
+        let p1 = pure (+1) <*> posInt
+        runParser p1 "41" `shouldBe` Just (42, "")
+
+        let p2 = (+) <$> posInt <* char ' ' <*> posInt
+        runParser p2 "12 13a" `shouldBe` Just (25, "a")
+
+  describe "abParser" $ do
+    it "parses the characters 'a' and 'b' as a pair" $ do
+      runParser abParser "abcdef" `shouldBe` Just (('a', 'b'), "cdef")
+      runParser abParser "bcdefa" `shouldBe` Nothing
+      runParser abParser "aecdbf" `shouldBe` Nothing
+
+  describe "abParser_" $ do
+    it "parses the characters 'a' and 'b' but returns nothing" $ do
+      runParser abParser_ "abcdef" `shouldBe` Just ((), "cdef")
+      runParser abParser_ "bcdefa" `shouldBe` Nothing
+      runParser abParser_ "aecdbf" `shouldBe` Nothing
+
+  describe "intPair" $ do
+    it "parses two integer values separated by a space" $ do
+      runParser intPair "12 34" `shouldBe` Just ([12, 34], "")
+
+  describe "Alternative Parser" $ do
+    describe "empty" $ do
+      it "is a parser that always fails" $ do
+        runParser (empty :: Parser ()) "abc" `shouldBe` Nothing
+
+    describe "<|>" $ do
+      it "uses the first parser if successful" $ do
+        let p1 = char '*' <|> char '$'
+        runParser p1 "*abc" `shouldBe` Just ('*', "abc")
+
+        let p2 = satisfy isNumber <|> (head . show <$> posInt)
+        runParser p2 "1234" `shouldBe` Just ('1', "234")
+
+      it "user the second parser if the first one fails" $ do
+        let p1 = char '*' <|> char '$'
+        runParser p1 "$abc" `shouldBe` Just ('$', "abc")
+
+        let p2 = satisfy isNumber <|> pure '*'
+        runParser p2 "abcd" `shouldBe` Just ('*', "abcd")
+
+        let p3 = satisfy isUpper <|> satisfy isNumber
+        runParser p3 "1234" `shouldBe` Just ('1', "234")
+        runParser p3 "*234" `shouldBe` Nothing
+
+  describe "intOrUppercase" $ do
+    it "consumes an integer or an uppercase character" $ do
+      runParser intOrUppercase "342abcd" `shouldBe` Just ((), "abcd")
+      runParser intOrUppercase "XYZ" `shouldBe` Just ((), "YZ")
+      runParser intOrUppercase "foo" `shouldBe` Nothing
+
+  let upper = satisfy isUpper
+
+  describe "zeroOrMore" $ do
+    it "runs the parser zero or more times" $ do
+      runParser (zeroOrMore upper) "ABCdEfgH" `shouldBe` Just ("ABC", "dEfgH")
+      runParser (zeroOrMore upper) "abcdeFGh" `shouldBe` Just ("", "abcdeFGh")
+
+  describe "oneOrMore" $ do
+    it "runs the parser one or more times" $ do
+      runParser (oneOrMore upper) "ABCdEfgH" `shouldBe` Just ("ABC", "dEfgH")
+      runParser (oneOrMore upper) "abcdeFGh" `shouldBe` Nothing
+
+  describe "spaces" $ do
+    it "parses a consecutive list of zero or more whitespace characters" $ do
+      runParser spaces "   a b c " `shouldBe` Just ("   ", "a b c ")
+      runParser spaces " \n \r \t a b c " `shouldBe` Just (" \n \r \t ", "a b c ")
+      runParser spaces "a b c " `shouldBe` Just ("", "a b c ")
+      runParser spaces "" `shouldBe` Just ("", "")
+
+  describe "ident" $ do
+    it "parses an alphabetic char followed by zero or more alphanumerics" $ do
+      runParser ident "foobar baz" `shouldBe` Just ("foobar", " baz")
+      runParser ident "foo33fA" `shouldBe` Just ("foo33fA", "")
+      runParser ident "2bad" `shouldBe` Nothing
+      runParser ident "" `shouldBe` Nothing
+
+  describe "parseSExpr" $ do
+    it "parses numeric atoms" $ do
+      runParser parseSExpr "5" `shouldBe` Just (A (N 5), "")
+
+    it "parses identifiers" $ do
+      runParser parseSExpr "foo3" `shouldBe` Just (A (I "foo3"), "")
+
+    it "parses lists of s-expressions" $ do
+      runParser parseSExpr "(bar (foo) 3 5 874)"
+        `shouldBe` Just (Comb [ A (I "bar"), Comb [A (I "foo")]
+                              , A (N 3), A (N 5), A (N 874)
+                              ], "")
+
+      runParser parseSExpr "(((lambda x (lambda y (plus x y))) 3) 5)"
+        `shouldBe` Just (Comb [ Comb [ Comb [ A (I "lambda"), A (I "x")
+                                     , Comb [ A (I "lambda"), A (I "y")
+                              , Comb [ A (I "plus"), A (I "x"), A (I "y") ]]]
+                              , A (N 3) ], A (N 5) ], "")
