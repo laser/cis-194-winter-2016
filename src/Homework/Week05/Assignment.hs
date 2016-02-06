0e134b3d2d80683e3c4227c36416cbbf9404f6b0
Vivek Sachdeva <vsachdeva@cj.com>
2016-02-06 14:15:44 -0800

    Finish assignment 5

--- src/Homework/Week05/Assignment.hs
+++ src/Homework/Week05/Assignment.hs
@@ -3,30 +3,66 @@ module Homework.Week05.Assignment (
   evalStr,
   ExprT(..),
   -- uncomment these once you've defined them:
-  -- Expr(..),
-  -- MinMax(..),
-  -- Mod7(..)
+  Expr(..),
+  MinMax(..),
+   Mod7(..)
 ) where
 
 import Homework.Week05.ExprT
 import Homework.Week05.Parser
+import Data.Maybe
 
 -- #1
 eval :: ExprT -> Integer
-eval = undefined
+eval (Lit x) = x
+eval (Add left right) =  (eval left) + (eval right)
+eval (Mul left right) = (eval left) * (eval right)
 
 -- #2
 evalStr :: String -> Maybe Integer
-evalStr = undefined
+evalStr input =
+  if parseExp Lit Add Mul input == Nothing
+    then Nothing
+  else Just(eval(fromJust(parseExp Lit Add Mul input)))
 
 -- #3
--- class Expr a where
---   lit :: ???
---   add :: ???
---   mul :: ???
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
 
 -- #4
--- instance Expr Integer where
---   lit = ???
---   add = ???
---   mul = ???
+instance Expr Integer where
+  lit a = a
+  add a b = a + b
+  mul a b = a * b
+
+instance Expr ExprT where
+  lit a = Lit a
+  add a b = Add a b
+  mul a b = Mul a b
+
+instance Expr Bool where
+  lit a =
+    if a > 0 then True
+    else False
+  add a b = a || b
+  mul a b = a && b
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit a = MinMax a
+  add (MinMax a) (MinMax b) =
+    if a > b then MinMax a
+    else MinMax b
+  mul (MinMax a) (MinMax b) =
+    if a > b then MinMax b
+    else MinMax a
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit a = Mod7 (a `mod` 7)
+  add (Mod7 a) (Mod7 b) = Mod7 ((a+b) `mod` 7)
+  mul (Mod7 a) (Mod7 b) = Mod7 ((a*b) `mod` 7)
