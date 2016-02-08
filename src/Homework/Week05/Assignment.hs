3d6ef94537c68d588c5d6f7c050056e55f72f5f1
Bill Adams <badams@cj.com>
2016-02-08 06:41:35 -0800

    assignment 5 up to ex 3

--- src/Homework/Week05/Assignment.hs
+++ src/Homework/Week05/Assignment.hs
@@ -8,6 +8,9 @@ module Homework.Week05.Assignment (
   Mod7(..)
  ,ExprT(..)
  ,Listable(..)
+ ,Expr(..)
+ ,reify
+ ,testInteger,testBool,testMM,testSat
 ) where
 
 import Homework.Week05.ExprT
@@ -43,21 +46,58 @@ instance (Listable a , Listable b) => Listable (a,b) where
 -- #1
 eval :: ExprT -> Integer
 eval (Lit a) =  a
-eval (Add a b) = (eval a)  + (eval b)
-eval (Mul a b) = (eval a)  * (eval b)
+eval (Add a b) = eval a + eval b
+eval (Mul a b) = eval a * eval b
 
 -- #2
 evalStr :: String -> Maybe Integer
-evalStr s = undefined -- parseExp Lit Add Mul s
+evalStr = fmap eval . parseExp Lit Add Mul
 
 -- #3
--- class Expr a where
---   lit :: ???
---   add :: ???
---   mul :: ???
+class Expr a where
+   lit :: Integer -> a
+   add :: a -> a -> a
+   mul :: a -> a -> a
 
 -- #4
--- instance Expr Integer where
---   lit = ???
---   add = ???
---   mul = ???
+instance Expr Integer where
+  lit =  id
+  add =  (+)
+  mul =  (*)
+
+instance Expr ExprT  where
+  lit  = Lit
+  add  = Add
+  mul  = Mul
+
+reify :: ExprT -> ExprT
+reify = id
+
+instance Expr Bool where
+  lit = (>0)
+  add = (||)
+  mul = (&&)
+
+newtype MinMax = MinMax Integer deriving (Eq,Show)
+newtype Mod7   = Mod7 Integer deriving (Eq,Show)
+
+instance Expr MinMax where
+  lit = MinMax
+  add (MinMax a) (MinMax b) = MinMax $ max a b
+  mul (MinMax a) (MinMax b) = MinMax $ min a b
+
+instance Expr Mod7 where
+  lit = Mod7
+  add (Mod7 a) (Mod7 b) = Mod7 $ mod ( a + b ) 7
+  mul (Mod7 a) (Mod7 b) = Mod7 $ mod ( a * b ) 7
+
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+
+-- # Execise 4 test
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
+
