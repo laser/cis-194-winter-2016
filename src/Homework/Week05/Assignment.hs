e3a6e3340369c3f8b355549782a920848910bd1a
Nicolas Font <nfont@cj.com>
2016-02-08 16:37:59 -0800

    Week 5

--- src/Homework/Week05/Assignment.hs
+++ src/Homework/Week05/Assignment.hs
@@ -3,9 +3,9 @@ module Homework.Week05.Assignment (
   evalStr,
   ExprT(..),
   -- uncomment these once you've defined them:
-  -- Expr(..),
-  -- MinMax(..),
-  -- Mod7(..)
+  Expr(..),
+  MinMax(..),
+  Mod7(..)
 ) where
 
 import Homework.Week05.ExprT
@@ -13,20 +13,50 @@ import Homework.Week05.Parser
 
 -- #1
 eval :: ExprT -> Integer
-eval = undefined
+eval (Lit x) = x
+eval (Add x y) = (eval x) + (eval y)
+eval (Mul x y) = (eval x) * (eval y)
 
 -- #2
 evalStr :: String -> Maybe Integer
-evalStr = undefined
+evalStr s = case (parseExp Lit Add Mul s) of
+  Nothing -> Nothing
+  Just expr -> Just (eval expr)
 
 -- #3
--- class Expr a where
---   lit :: ???
---   add :: ???
---   mul :: ???
+class Expr a where
+  lit :: Integer -> a
+  add :: a -> a -> a
+  mul :: a -> a -> a
+
+instance Expr ExprT where
+  lit x = Lit x
+  add x y = Add x y
+  mul x y = Mul x y
 
 -- #4
--- instance Expr Integer where
---   lit = ???
---   add = ???
---   mul = ???
+instance Expr Integer where
+  lit x = x
+  add x y = x + y
+  mul x y = x * y
+
+instance Expr Bool where
+  lit x = x > 0
+  add x y = x || y
+  mul x y = x && y
+
+data MinMax = MinMax Integer
+  deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit x = MinMax x
+  add (MinMax x) (MinMax y) = MinMax (max x y)
+  mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+data Mod7 = Mod7 Integer
+  deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit x = Mod7 $ x `mod` 7
+  add (Mod7 x) (Mod7 y) = Mod7 $ (x + y) `mod` 7
+  mul (Mod7 x) (Mod7 y) = Mod7 $ (x * y) `mod` 7
