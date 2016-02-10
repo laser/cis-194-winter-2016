b52658a178473f38051041e55e07b8e3bfedff25
Adams <bill_t_adams@hotmail.com>
2016-02-10 07:34:39 -0800

    rm junk in checkin

--- src/Homework/Week06/Assignment.hs
+++ src/Homework/Week06/Assignment.hs
@@ -4,7 +4,7 @@ module Homework.Week06.Assignment (
   fibs2,
   streamToList,
   streamRepeat,
-  streamMap,
+  streamMap, 
   streamFromSeed,
   nats,
   ruler,
@@ -19,12 +19,33 @@ fib n =  (fib $ n - 1)  + (fib $ n - 2)
 
 fibs1 :: [Integer]
 fibs1 = fmap fib [0..]
+--
+-- try this one with iterate
+--
 
 -- #2
 fibs2 :: [Integer]
-fibs2 = erro "undefined"
+fibs2 = error "undefined"
 
+-- to be O(n) we have to retain previous calculations
+-- so recursion passing a map or container is what we need to do
+-- and we cant compute things going off in two par recursions because we have to share the same map/container
+-- goFib ( n , [(0,0),(1,1)] ).....
+-- goFib n priors =  lookup n priors
+--                   if found in priors then done
+--                   otherwise compute fib for n-2 and then n-1  storing both in priors container
+
+--
+-- given n and map
+-- lookup n in map
+-- if found use n
+-- else ....??? what now???
+--   store n -> n-1 + n -2
+--
+
+--
 -- #3
+--
 data Stream a = Stream a -- replace this with your own definition; this one is wrong
 
 streamToList :: Stream a -> [a]
