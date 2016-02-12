110110dc284aecff4db3f5acc082e135f77554fd
billtuba <bill_t_adams@hotmail.com>
2016-02-12 14:51:08 -0800

    Assignment 6 - Thru Ex 5

--- src/Homework/Week06/Assignment.hs
+++ src/Homework/Week06/Assignment.hs
@@ -24,21 +24,10 @@ fib n =  (fib $ n - 1)  + (fib $ n - 2)
 fibs1 :: [Integer]
 fibs1 = fmap fib [0..]
 --
--- try this one with iterate
--- ooooo
 
 -- #2
 fibs2 :: [Integer]
 fibs2 = poorslidinfibz
-
--- to be O(n) we have to retain previous calculations
--- so recursion passing a map or container is what we need to do
--- and we cant compute things going off in two par recursions because we have to share the same map/container
--- goFib ( n , [(0,0),(1,1)] ).....
--- goFib n priors =  lookup n priors
---                   if found in priors then done
---                   otherwise compute fib for n-2 and then n-1  storing both in priors container
-
 
 -- poppin fibz the hard way... oh yeah!
 rubegoldfibz :: Int -> [Integer]
@@ -53,37 +42,36 @@ poorslidinfibz = fmap snd $ go [(0,0),(1,1)]
                                         where last2 = take 2 $ drop (length all - 2)  all
                                               nextFibs [(a,b),(c,d)]= [(succ c, b + d)]
 
--- given n and map
--- lookup n in map
--- if found use n
--- else ....??? what now???
---   store n -> n-1 + n -2
---
-
 --
 -- #3
 --
-data Stream a = Stream a -- replace this with your own definition; this one is wrong
+data Stream a = Cons a (Stream a)
 
 streamToList :: Stream a -> [a]
-streamToList = undefined
+streamToList (Cons a s) = a : streamToList s
 
--- instance Show a => Show (Stream a) where
---   show = ???
+instance Show a => Show (Stream a) where
+   show (Cons a s)= (show a) ++ "," ++ (show s)
 
 -- #4
 streamRepeat :: a -> Stream a
-streamRepeat = undefined
+streamRepeat a = Cons a $ streamRepeat a
 
 streamMap :: (a -> b) -> Stream a -> Stream b
-streamMap = undefined
+streamMap f (Cons a s) = Cons (f a) $ streamMap f s
 
 streamFromSeed :: (a -> a) -> a -> Stream a
-streamFromSeed = undefined
+streamFromSeed f e = Cons e $ streamFromSeed f (f e)
 
 -- #5
 nats :: Stream Integer
-nats = undefined
+nats = streamFromSeed (+1) 0
 
 ruler :: Stream Integer
-ruler = undefined
+ruler = undefined -- streamMap (\ x->
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams  a b = undefined
+                        --where go (Cons e1 s1) (Cons e2 s2) = Cons e1
+
+
