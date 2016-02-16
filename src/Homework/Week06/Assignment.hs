fc0633d4540158857f32a866b7e4811aa3cf817b
Nicolas Font <nfont@cj.com>
2016-02-15 17:22:31 -0800

    Week 6

--- src/Homework/Week06/Assignment.hs
+++ src/Homework/Week06/Assignment.hs
@@ -13,37 +13,44 @@ module Homework.Week06.Assignment (
 
 -- #1a
 fib :: Integer -> Integer
-fib = undefined
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
 
 fibs1 :: [Integer]
-fibs1 = undefined
+fibs1 = map fib [0..]
 
 -- #2
 fibs2 :: [Integer]
-fibs2 = undefined
+fibs2 = [0,1] ++ fibs2' 0 1 [2..] where
+  fibs2' n1 n2 (_:xs) = [(n1 + n2)] ++ fibs2' n2 (n1 + n2) xs
 
 -- #3
-data Stream a = Stream a -- replace this with your own definition; this one is wrong
+--data Stream a = Stream a -- replace this with your own definition; this one is wrong
+data Stream a = Cons a (Stream a)
 
 streamToList :: Stream a -> [a]
-streamToList = undefined
+streamToList (Cons x s) = x : streamToList s
 
--- instance Show a => Show (Stream a) where
---   show = ???
+instance Show a => Show (Stream a) where
+  show s = show $ take 20 $ streamToList s
 
 -- #4
 streamRepeat :: a -> Stream a
-streamRepeat = undefined
+streamRepeat s = Cons s (streamRepeat s)
 
 streamMap :: (a -> b) -> Stream a -> Stream b
-streamMap = undefined
+streamMap f (Cons x s) = Cons (f x) (streamMap f s)
 
 streamFromSeed :: (a -> a) -> a -> Stream a
-streamFromSeed = undefined
+streamFromSeed f x = Cons x (streamFromSeed f (f x))
 
 -- #5
 nats :: Stream Integer
-nats = undefined
+nats = streamFromSeed (+1) 0
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons x xs) ys = Cons x $ interleaveStreams ys xs
 
 ruler :: Stream Integer
-ruler = undefined
+ruler = interleaveStreams (streamRepeat 0) (streamMap (+1) ruler)
