6d68d517cf562b64689ce6a50072999f1711ef0c
marnold <michaelaarnold@gmail.com>
2016-02-16 11:03:36 -0800

    wk 6

--- src/Homework/Week06/Assignment.hs
+++ src/Homework/Week06/Assignment.hs
@@ -6,10 +6,12 @@ module Homework.Week06.Assignment (
   streamRepeat,
   streamMap,
   streamFromSeed,
-  nats,
   ruler,
+  nats,
   Stream(..)
 ) where
+
+import           Data.List
 
 -- #1a
 fib :: Integer -> Integer
@@ -22,30 +24,46 @@ fibs1 = map fib [0..]
 
 -- #2
 fibs2 :: [Integer]
-fibs2 = undefined
+fibs2 = 0:1:zipWith (+) fibs2 (tail fibs2)
 
 -- #3
-data Stream a = Stream a -- replace this with your own definition; this one is wrong
+data Stream a = Conz a (Stream a)
 
 streamToList :: Stream a -> [a]
-streamToList = undefined
+streamToList (Conz x xs) = x : streamToList xs
 
--- instance Show a => Show (Stream a) where
---   show = ???
+instance Show a => Show (Stream a) where
+  show s = show $ take 10 $ streamToList s
 
 -- #4
 streamRepeat :: a -> Stream a
-streamRepeat = undefined
+streamRepeat x = Conz x $ streamRepeat x
 
 streamMap :: (a -> b) -> Stream a -> Stream b
-streamMap = undefined
+streamMap fn (Conz x xs) = Conz (fn x) $ streamMap fn xs
 
 streamFromSeed :: (a -> a) -> a -> Stream a
-streamFromSeed = undefined
+streamFromSeed fn x = Conz x $ streamFromSeed fn $ fn x
 
 -- #5
-nats :: Stream Integer
-nats = undefined
+nats :: Stream Int
+nats = streamFromSeed (+1) 0
 
-ruler :: Stream Integer
-ruler = undefined
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams ~(Conz x xs) ~(Conz y ys) = Conz x $ Conz y $ interleaveStreams xs ys
+--tildes force lazy pattern matching. Without them, ruler never terminates
+
+rulerBuilder :: Int -> Stream Int
+rulerBuilder n = interleaveStreams (streamRepeat n) $ rulerBuilder (n+1)
+
+ruler :: Stream Int
+ruler = rulerBuilder 0
+
+-- how this works:
+-- ruler = interleaveStreams zeros as
+--   where zeros = streamRepeat 0
+--         as    = interleaveStreams ones bs
+--         ones  = streamRepeat 1
+--         bs    = interleaveStreams twos cs
+--         twos  = streamRepeat 2
+--         cs    = interleaveStreams (streamRepeat 3) (streamRepeat 4)
