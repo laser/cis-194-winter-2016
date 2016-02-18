ff54d2b59122dbd609509f207b788a3480989503
Erin Swenson-Healey <eswenson-healey@cj.com>
2016-02-18 10:46:15 -0800

    Add 6th week impl

--- src/Homework/Week06/Assignment.hs
+++ src/Homework/Week06/Assignment.hs
@@ -11,39 +11,127 @@ module Homework.Week06.Assignment (
   Stream(..)
 ) where
 
--- #1a
+----------
+-- Ex 1 --
+----------
+
+-- Translate the above definition of Fibonacci numbers directly into a
+-- recursive function definition of type:
+--
 fib :: Integer -> Integer
-fib = undefined
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+--
+-- so that fib n computes the nth Fibonacci number Fn. Then, use fib to
+-- define the infinite list of all Fibonacci numbers:
+--
+-- fibs1 :: [Integer]
 
 fibs1 :: [Integer]
-fibs1 = undefined
+fibs1 = map fib [0..]
 
--- #2
+----------
+-- Ex 2 --
+----------
+
+-- Define the infinite list:
+--
+-- fibs2 :: [Integer]
+--
+-- so that it has the same elements as fibs1, but computing the first n
+-- elements of fibs2 requires only O(n) additional operations. Be sure to
+-- use standard recursion pattern(s) from Prelude, as appropriate.
+--
+
 fibs2 :: [Integer]
-fibs2 = undefined
+fibs2 = map head $ iterate (\(a:b:c:ds) -> [b, c, (b+c)]) [0, 1, 1]
 
--- #3
-data Stream a = Stream a -- replace this with your own definition; this one is wrong
+----------
+-- Ex 3 --
+----------
+
+-- * Define a data type of polymorphic streams, Stream.
+
+data Stream a = Cons a (Stream a)
+
+-- * Write a function to convert a Stream to an infinite list:
+--
+--   streamToList :: Stream a -> [a]
 
 streamToList :: Stream a -> [a]
-streamToList = undefined
+streamToList (Cons x c) = x : streamToList c
 
--- instance Show a => Show (Stream a) where
---   show = ???
+--
+-- * Make your own instance of Show for Stream:
+--
+instance Show a => Show (Stream a) where
+  show s = show $ take 20 $ streamToList s
+--
+--   ...which works by showing only some prefix of a stream (say, the first
+--   20 elements)
 
--- #4
+----------
+-- Ex 4 --
+----------
+
+-- * Write a function:
+--
+-- ...which generates a stream containing infinitely many copies of the
+-- given element
 streamRepeat :: a -> Stream a
-streamRepeat = undefined
+streamRepeat x = Cons x $ streamRepeat x
 
+-- * Write a function:
+--
+-- ...which applies a function to every element of a Stream
 streamMap :: (a -> b) -> Stream a -> Stream b
-streamMap = undefined
+streamMap f (Cons x s) = Cons (f x) $ streamMap f s
 
+-- * Write a function:
+--
+-- ...which generates a Stream from a “seed” of type a, which is the first
+-- element of the stream, and an “unfolding rule” of type a -> a which
+-- specifies how to transform the seed into a new seed, to be used for
+-- generating the rest of the stream.
 streamFromSeed :: (a -> a) -> a -> Stream a
-streamFromSeed = undefined
+streamFromSeed f x = Cons x $ streamFromSeed f (f x)
 
--- #5
+----------
+-- Ex 5 --
+----------
+
+-- * Define the stream:
+--
 nats :: Stream Integer
-nats = undefined
+nats = streamFromSeed (+1) (0 :: Integer)
+--
+-- ...which contains the infinite list of natural numbers 0, 1, 2...
+--
+-- * Define the stream:
+--
+-- ruler :: Stream Integer
+--
+-- ...which corresponds to the ruler function:
+--
+-- 0,1,0,2,0,1,0,3,0,1,0,2,0,1,0,4,...
+--
+-- ...where the nth element in the stream (assuming the first element
+-- corresponds to n = 1) is the largest power of 2 which evenly divides n.
+--
+-- Hint: define a function interleaveStreams which alternates the
+-- elements from two streams. Can you use this function to implement ruler
+-- in a clever way that does not have to do any divisibility testing?
+
+interleaveStreams :: Stream a -> Stream a -> Stream a
+interleaveStreams (Cons x1 s1) (Cons x2 s2) = Cons x1 $ Cons x2 $ interleaveStreams s1 s2
 
 ruler :: Stream Integer
-ruler = undefined
+ruler = interleaveStreams (streamRepeat 0) (streamMap highestPowOfTwoThatDivsEvenly $ streamFromSeed (+2) 2)
+
+highestPowOfTwoThatDivsEvenly :: Integer -> Integer
+highestPowOfTwoThatDivsEvenly n = go 1
+  where
+    go acc = if isInt (quotient (acc + 1)) then go (acc + 1) else acc
+    isInt x = x == fromInteger (round x)
+    quotient pow = (fromIntegral n) / 2**(fromIntegral pow)
