6662576d41b70949def3429be7e74a50eb4c4a20
marnold <michaelaarnold@gmail.com>
2016-02-29 19:36:47 -0800

    uncomment wk 1 tests

--- test/Homework/Week01Spec.hs
+++ test/Homework/Week01Spec.hs
@@ -14,29 +14,23 @@ spec :: Spec
 spec = do
   describe "toDigits" $ do
     it "should split digits of integer into a list" $ do
-      pending
       toDigits 1234 `shouldBe` [1,2,3,4]
 
     it "should return an empty list for zero" $ do
-      pending
       toDigits 0 `shouldBe` []
 
     it "should return an empty list for negative numbers" $ do
-      pending
       toDigits (-1) `shouldBe` []
 
   describe "toDigitsRev" $ do
     it "should return an empty list for zero" $ do
-      pending
       toDigitsRev 0 `shouldBe` []
 
     it "should return an empty list for negative numbers" $ do
-      pending
       toDigitsRev (-1) `shouldBe` []
       toDigitsRev (-22222) `shouldBe` []
 
     it "should split digits of integer into a list in reverse order" $ do
-      pending
       toDigitsRev 123 `shouldBe` [3,2,1]
       toDigitsRev 431 `shouldBe` [1,3,4]
       toDigitsRev 12 `shouldBe` [2,1]
@@ -44,27 +38,22 @@ spec = do
 
   describe "doubleEveryOther" $ do
     it "should return an empty list given an empty list" $ do
-      pending
       doubleEveryOther [] `shouldBe` []
 
     it "should double every other int in the list, from right to left" $ do
-      pending
       doubleEveryOther [8,7,6,5] `shouldBe`[16,7,12,5]
       doubleEveryOther [1,2,3] `shouldBe` [1,4,3]
 
   describe "sumDigits" $ do
     it "should return zero for an empty list" $ do
-      pending
       sumDigits [] `shouldBe` 0
 
     it "should sum all digits in the list" $ do
-      pending
       sumDigits [16,7,12,5] `shouldBe` 22
       sumDigits [18,7,33,5] `shouldBe` 27
 
   describe "validate" $ do
     it "should return True for valid card number" $ do
-      pending
       validate 4012888888881881 `shouldBe` True
 
     it "should return False for invalid card number" $ do
