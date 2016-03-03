ea29e2f7af7ca1c372a5fe080faa9704d1208840
Erin Swenson-Healey <eswenson-healey@cj.com>
2016-03-03 11:24:21 -0800

    Slight modification to the test to poke CI

--- test/Homework/Week01Spec.hs
+++ test/Homework/Week01Spec.hs
@@ -14,7 +14,7 @@ spec :: Spec
 spec = do
   describe "toDigits" $ do
     it "should split digits of integer into a list" $ do
-      toDigits 1234 `shouldBe` [1,2,3,4]
+      toDigits 12345 `shouldBe` [1,2,3,4,5]
 
     it "should return an empty list for zero" $ do
       toDigits 0 `shouldBe` []
