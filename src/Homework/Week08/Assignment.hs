cae72392abf113070259a12cba7e2324943867cf
Nicolas Font <nfont@cj.com>
2016-03-04 15:59:55 -0800

    Week 8

--- src/Homework/Week08/Assignment.hs
+++ src/Homework/Week08/Assignment.hs
@@ -8,34 +8,43 @@ module Homework.Week08.Assignment (
 
 import Homework.Week08.AParser
 import Control.Applicative
+import Data.Char
 
 -- #1
 first :: (a -> b) -> (a,c) -> (b,c)
-first = undefined
+first f (x,y) = (f x, y)
 
 instance Functor Parser where
-  fmap = undefined
+  fmap f p = Parser $ \s -> first f <$> runParser p s
 
 -- #2
+
 instance Applicative Parser where
-  pure = undefined
-  _ <*> _ = undefined
+  pure x = Parser $ \s -> Just (x, s)
+  p1 <*> p2 = Parser $ \s -> do
+    (f, s1) <- runParser p1 s
+    (x, s2) <- runParser p2 s1
+    return (f x, s2)
 
 -- #3
 abParser :: Parser (Char, Char)
-abParser = undefined
+abParser = (,) <$> char 'a' <*> char 'b'
 
 abParser_ :: Parser ()
-abParser_ = undefined
+abParser_ = const () <$> abParser
 
 intPair :: Parser [Integer]
-intPair = undefined
+intPair = Parser $ \s -> do
+  (x1, s1) <- runParser posInt s
+  (_, s2) <- runParser (char ' ') s1
+  (x2, s3) <- runParser posInt s2
+  return ([x1,x2], s3)
 
 -- #4
 instance Alternative Parser where
-  empty = undefined
-  _ <|> _ = undefined
+  empty = Parser $ const Nothing
+  p1 <|> p2 = Parser $ \s -> runParser p1 s <|> runParser p2 s
 
 -- #5
 intOrUppercase :: Parser ()
-intOrUppercase = undefined
+intOrUppercase = const () <$> posInt <|> const () <$> satisfy isUpper
