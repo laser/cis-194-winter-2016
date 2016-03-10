f11899e8e55113bde19f7125e9abc62ba3e493d5
Mary Flagstad <mflagstad@cj.com>
2016-03-10 12:48:35 -0800

    week 08

--- src/Homework/Week08/Assignment.hs
+++ src/Homework/Week08/Assignment.hs
@@ -8,34 +8,56 @@ module Homework.Week08.Assignment (
 
 import Homework.Week08.AParser
 import Control.Applicative
+import Data.Maybe
+import Data.Char
+import Control.Monad
 
 -- #1
 first :: (a -> b) -> (a,c) -> (b,c)
-first = undefined
+first f (a,c) = ((f a), c)
 
 instance Functor Parser where
-  fmap = undefined
+  fmap fn (Parser g) = Parser f where
+    f = fmap (first fn) . g
 
 -- #2
 instance Applicative Parser where
-  pure = undefined
-  _ <*> _ = undefined
+  pure empty = Parser f
+    where f str = Just (empty, str)
+
+  p1@(Parser f1) <*> p2@(Parser f2) = Parser f
+    where
+      f str = case f1 str of
+        Nothing -> Nothing
+        Just (aTob, str2) ->
+          case f2 str2 of
+            Nothing -> Nothing
+            thing -> fmap (first aTob) thing
 
 -- #3
 abParser :: Parser (Char, Char)
-abParser = undefined
+abParser = (,) <$> char 'a' <*> char 'b'
 
 abParser_ :: Parser ()
-abParser_ = undefined
+abParser_ = pure f <*> abParser
+  where
+    f _ = ()
 
 intPair :: Parser [Integer]
-intPair = undefined
+intPair = f <$> posInt <* char ' '<*> posInt
+  where
+    f x y = x:y:[]
 
 -- #4
 instance Alternative Parser where
-  empty = undefined
-  _ <|> _ = undefined
+  empty = Parser f where
+    f _ = Nothing
+  p1@(Parser f1) <|> p2@(Parser f2) = Parser f
+    where
+      f str = (f1 str) <|> (f2 str)
+
 
 -- #5
+
 intOrUppercase :: Parser ()
-intOrUppercase = undefined
+intOrUppercase = void posInt <|> void (satisfy isUpper)
