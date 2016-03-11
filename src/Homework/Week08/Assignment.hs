cfc03a62654216dfd8dc4d87e155bbc9c1c7b3ab
Abdussalam Abdurrahman <abdusalam@live.com>
2016-03-10 23:21:21 -0800

    week 8: clean-up

--- src/Homework/Week08/Assignment.hs
+++ src/Homework/Week08/Assignment.hs
@@ -15,14 +15,14 @@ first :: (a -> b) -> (a, c) -> (b, c)
 first f (x, y) = (f x, y)
 
 instance Functor Parser where
-  fmap f p = Parser (fp f p)
-    where fp f p = \ xs -> (first f) <$> (runParser p xs)
+  fmap f p = Parser fp
+    where fp xs = (first f) <$> (runParser p xs)
 
 -- #2
 instance Applicative Parser where
-  pure a = Parser (\ xs -> Just (a, xs))
-  ff <*> fa = Parser f
-    where f xs = maybe Nothing (\ (h, ys) -> runParser (h <$> fa) ys) (runParser ff xs)
+  pure x    = Parser (\xs -> Just (x, xs))
+  p1 <*> p2 = Parser f
+    where f xs = maybe Nothing (\(g, ys) -> runParser (g <$> p2) ys) (runParser p1 xs)
 
 -- #3
 abParser :: Parser (Char, Char)
@@ -30,14 +30,14 @@ abParser = f <$> (char 'a') <*> (char 'b')
   where f x y = (x, y)
 
 abParser_ :: Parser ()
-abParser_ = (\ _ -> ()) <$> abParser
+abParser_ = (\_ -> ()) <$> abParser
 
 intPair :: Parser [Integer]
-intPair = (\ x _ y -> [x, y]) <$> posInt <*> (char ' ') <*> posInt
+intPair = (\x _ y -> [x, y]) <$> posInt <*> (char ' ') <*> posInt
 
 -- #4
 instance Alternative Parser where
-  empty = Parser (\ _ -> Nothing)
+  empty     = Parser (\_ -> Nothing)
   p1 <|> p2 = Parser p
     where p xs = maybe (runParser p2 xs) Just (runParser p1 xs)
 
