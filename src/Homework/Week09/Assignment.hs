29d518511e74fb28de0b6caa9180a1a3286800f4
marnold <michaelaarnold@gmail.com>
2016-03-17 11:24:59 -0700

    week 9 hw

--- src/Homework/Week09/Assignment.hs
+++ src/Homework/Week09/Assignment.hs
@@ -9,23 +9,35 @@ module Homework.Week09.Assignment (
   SExpr(..)
 ) where
 
-import Control.Applicative
-
-import Homework.Week09.AParser
+import           Control.Applicative
+import           Data.Char
+import           Homework.Week09.AParser
 
 -- #1
 zeroOrMore :: Parser a -> Parser [a]
-zeroOrMore p = undefined
+-- zeroOrMore p = anyP
+--   where anyP = someP <|> pure []
+--         someP = (fmap (:) p) <*> anyP
+zeroOrMore p = oneOrMore p <|> pure []
+
+--breakdown: Parser(p : (p : (p : (p : (someP <|> pure [])  <|> pure [])  <|> pure [])  <|> pure []) )
 
 oneOrMore :: Parser a -> Parser [a]
-oneOrMore p = undefined
+-- oneOrMore p = someP
+--   where anyP = someP <|> pure []
+--         someP = (fmap (:) p) <*> anyP
+
+-- oneOrMore p = (:) <$> p <*> zeroOrMore p
+oneOrMore p = liftA2 (:) p (zeroOrMore p)
+
 
 -- #2
 spaces :: Parser String
-spaces = undefined
+spaces = zeroOrMore (satisfy isSpace)
 
 ident :: Parser String
-ident = undefined
+-- ident = (:) <$> satisfy isAlpha <*> zeroOrMore (satisfy isAlphaNum)
+ident = liftA2 (:) (satisfy isAlpha) (zeroOrMore (satisfy isAlphaNum))
 
 -- #3
 type Ident = String
@@ -39,4 +51,11 @@ data SExpr = A Atom
   deriving (Eq, Show)
 
 parseSExpr :: Parser SExpr
-parseSExpr = undefined
+parseSExpr = stripSpaces
+  where stripSpaces = spaces *> p <* spaces
+        p = atomP <|> combP
+        atomP = intP <|> identP
+        intP = A . N <$> posInt
+        identP = A . I <$> ident
+        combP = Comb <$> sExprP
+        sExprP = char '(' *> oneOrMore parseSExpr <* char ')'
--- test/Homework/Week09Spec.hs
+++ test/Homework/Week09Spec.hs
@@ -3,12 +3,12 @@ module Homework.Week09Spec (
   spec
 ) where
 
-import Test.Hspec
+import           Test.Hspec
 
-import Homework.Week09.AParser
-import Homework.Week09.Assignment
+import           Homework.Week09.AParser
+import           Homework.Week09.Assignment
 
-import Data.Char (isUpper)
+import           Data.Char                  (isUpper)
 
 main :: IO ()
 main = hspec spec
@@ -19,19 +19,16 @@ spec = do
 
   describe "zeroOrMore" $ do
     it "runs the parser zero or more times" $ do
-      pending
       runParser (zeroOrMore upper) "ABCdEfgH" `shouldBe` Just ("ABC", "dEfgH")
       runParser (zeroOrMore upper) "abcdeFGh" `shouldBe` Just ("", "abcdeFGh")
 
   describe "oneOrMore" $ do
     it "runs the parser one or more times" $ do
-      pending
       runParser (oneOrMore upper) "ABCdEfgH" `shouldBe` Just ("ABC", "dEfgH")
       runParser (oneOrMore upper) "abcdeFGh" `shouldBe` Nothing
 
   describe "spaces" $ do
     it "parses a consecutive list of zero or more whitespace characters" $ do
-      pending
       runParser spaces "   a b c " `shouldBe` Just ("   ", "a b c ")
       runParser spaces " \n \r \t a b c " `shouldBe` Just (" \n \r \t ", "a b c ")
       runParser spaces "a b c " `shouldBe` Just ("", "a b c ")
@@ -39,7 +36,6 @@ spec = do
 
   describe "ident" $ do
     it "parses an alphabetic char followed by zero or more alphanumerics" $ do
-      pending
       runParser ident "foobar baz" `shouldBe` Just ("foobar", " baz")
       runParser ident "foo33fA" `shouldBe` Just ("foo33fA", "")
       runParser ident "2bad" `shouldBe` Nothing
@@ -47,15 +43,12 @@ spec = do
 
   describe "parseSExpr" $ do
     it "parses numeric atoms" $ do
-      pending
       runParser parseSExpr "5" `shouldBe` Just (A (N 5), "")
 
     it "parses identifiers" $ do
-      pending
       runParser parseSExpr "foo3" `shouldBe` Just (A (I "foo3"), "")
 
     it "parses lists of s-expressions" $ do
-      pending
       runParser parseSExpr "(bar (foo) 3 5 874)"
         `shouldBe` Just (Comb [ A (I "bar"), Comb [A (I "foo")]
                               , A (N 3), A (N 5), A (N 874)
