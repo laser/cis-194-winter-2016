55d52b9a172f85e47f81d6b7881f52e24e75f069
Erin Swenson-Healey <eswenson-healey@cj.com>
2016-03-17 12:55:35 -0700

    Week 08 and 09

--- src/Homework/Week08/Assignment.hs
+++ src/Homework/Week08/Assignment.hs
@@ -8,34 +8,88 @@ module Homework.Week08.Assignment (
 
 import Homework.Week08.AParser
 import Control.Applicative
+import Data.Char (isUpper)
 
--- #1
-first :: (a -> b) -> (a,c) -> (b,c)
-first = undefined
+------------------------------------------------------------
+-- Your code goes below here
+------------------------------------------------------------
+
+-- Ex. 1 - implement a Functor instance for Parser
+
+first :: (a -> b) -> (a, c) -> (b, c)
+first f (x, y) = (f x, y)
 
 instance Functor Parser where
-  fmap = undefined
+  fmap f1 p = Parser f2
+    where
+      f2 xs = case runParser p xs of
+        Nothing  -> Nothing
+        (Just v) -> Just $ first f1 v
 
--- #2
+-- Ex. 2 - implement an Applicative instance for Parser
+--
+-- A. pure a represents the parser which consumes no input
+--    and successfully returns a result of a.
+--
+-- B. p1 <*> p2 represents the parser which ﬁrst runs p1
+--    (which will consume some input and produce a
+--    function), then passes the remaining input to p2
+--    (which consumes more input and produces some value),
+--    then returns the result of applying the function to
+--    the value. However, if either p1 or p2 fails then the
+--    whole thing should also fail (put another way, p1 <*>
+--    p2 only succeeds if both p1 and p2 succeed).
+
 instance Applicative Parser where
-  pure = undefined
-  _ <*> _ = undefined
+  pure x = Parser $ \s -> Just (x, s)
+  (Parser p1) <*> p2 = Parser $ \s -> case p1 s of
+    Nothing        -> Nothing
+    (Just (f, xs)) -> runParser (f <$> p2) xs
 
--- #3
+-- Ex. 3a - Create a parser which expects to see the
+-- characters ’a’ and ’b’ and returns them as a pair
+
 abParser :: Parser (Char, Char)
-abParser = undefined
+abParser = (\x y -> (x, y)) <$> char 'a' <*> char 'b'
 
+-- Ex. 3b - Create a parser which acts in the same way as
+-- abParser but returns () instead of 'a' and 'b'
 abParser_ :: Parser ()
-abParser_ = undefined
+abParser_ = (\x y -> ()) <$> char 'a' <*> char 'b'
 
-intPair :: Parser [Integer]
-intPair = undefined
+-- Ex. 3c - Create a parser which reads two integer values
+-- separated by a space and returns the integer values in a
+-- list. You should use the provided posInt to parse the
+-- integer values.
 
--- #4
+intPair :: Parser ([Integer])
+intPair = (\x y z -> x:z:[]) <$> posInt <*> char ' ' <*> posInt
+
+-- Ex. 4 - Write an Alternative instance for Parser
+--
+-- See: http://hackage.haskell.org/package/base-4.7.0.0/docs/Control-Applicative.html#t:Alternative
+--
+-- empty represents the parser which always fails.
+-- p1 <|> p2 represents the parser which ﬁrst tries running
+-- p1. If p1 succeeds then p2 is ignored and the result of
+-- p1 is returned.  Otherwise, if p1 fails, then p2 is
+-- tried instead.
+--
+-- Hint: there is already an Alternative instance for Maybe
+-- which you may find useful.
+
 instance Alternative Parser where
-  empty = undefined
-  _ <|> _ = undefined
+  empty = Parser (\s -> Nothing)
+  p1 <|> p2 = Parser $ \s -> case runParser p1 s of
+    Nothing -> runParser p2 s
+    x       -> x
 
--- #5
+-- Ex. 5 - Implement a parser:
+--
+-- intOrUppercase :: Parser ()
+--
+-- which parses either an integer value or an uppercase
+-- character, and fails otherwise.
+
 intOrUppercase :: Parser ()
-intOrUppercase = undefined
+intOrUppercase = const () <$> satisfy isUpper <|> const () <$> posInt
--- src/Homework/Week09/Assignment.hs
+++ src/Homework/Week09/Assignment.hs
@@ -10,33 +10,87 @@ module Homework.Week09.Assignment (
 ) where
 
 import Control.Applicative
+import Data.Char (isAlpha, isAlphaNum)
 
 import Homework.Week09.AParser
 
--- #1
-zeroOrMore :: Parser a -> Parser [a]
-zeroOrMore p = undefined
+------------------------------------------------------------
+--  1. Parsing repetitions
+------------------------------------------------------------
+
+-- Hint: To parse one or more occurrences of p, run p once
+-- and then parse zero or more occurrences of p.
 
 oneOrMore :: Parser a -> Parser [a]
-oneOrMore p = undefined
+oneOrMore = some
+{-oneOrMore p = (:) <$> p <*> zeroOrMore p-}
 
--- #2
+-- To parse zero or more occurrences of p, try parsing one
+-- or more; if that fails, return the empty list.
+
+zeroOrMore :: Parser a -> Parser [a]
+zeroOrMore = many
+{-zeroOrMore p = oneOrMore p <|> pure []-}
+
+------------------------------------------------------------
+--  2. Utilities
+------------------------------------------------------------
+
+-- First, spaces should parse a consecutive list of zero or
+-- more whitespace characters.
+
 spaces :: Parser String
-spaces = undefined
+spaces = many $ (char ' ' <|> char '\n' <|> char '\t' <|> char '\r')
+
+-- Next, ident should parse an identifier, which for our
+-- purposes will be an alphabetic character (use isAlpha)
+-- followed by zero or more alphanumeric characters (use
+-- isAlphaNum).
 
 ident :: Parser String
-ident = undefined
+ident = (:) <$> (satisfy isAlpha) <*> many (satisfy isAlphaNum)
 
--- #3
+------------------------------------------------------------
+--  3. Parsing S-expressions
+------------------------------------------------------------
+
+-- An "identifier" is represented as just a String; however, only
+-- those Strings consisting of a letter followed by any number of
+-- letters and digits are valid identifiers.
 type Ident = String
 
+-- An "atom" is either an integer value or an identifier.
 data Atom = N Integer
           | I Ident
-  deriving (Eq, Show)
+  deriving (Show, Eq)
 
+-- An S-expression is either an atom, or a list of S-expressions.
 data SExpr = A Atom
            | Comb [SExpr]
-  deriving (Eq, Show)
+  deriving (Show, Eq)
+
+-- Textually, S-expressions can optionally begin and end
+-- with any number of spaces; after throwing away leading
+-- and trailing spaces they consist of either an atom, or
+-- an open parenthesis followed by one or more S-expressions
+-- followed by a close parenthesis.
+--
+-- For example, the following are all valid S-expressions:
+--  5
+--  foo3
+--  (bar (foo) 3 5 874)
+--  (((lambda x (lambda y (plus x y))) 3) 5)
+--  (   lots  of   (  spaces   in  )  this ( one ) )
+
+parseAtom :: Parser Atom
+parseAtom = (pure (N) <*> posInt) <|> (pure (I) <*> ident)
 
 parseSExpr :: Parser SExpr
-parseSExpr = undefined
+parseSExpr = spaces *> parse <* spaces
+  where parse = pure (A) <*> parseAtom <|> (pure (Comb) <*> (char '(' *> some parseSExpr <* char ')'))
+
+sumParse :: Parser Integer
+sumParse = spaces *> (hit <|> miss) <* spaces
+  where
+    hit  = posInt
+    miss = foldl1 (+) <$> (char '(' *> some sumParse <* char ')')
